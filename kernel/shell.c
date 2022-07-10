bc3b3f44dcd1851176777810df4c1dcc0b9c7d4b
pf4 <74263128+elydre@users.noreply.github.com>
2022-07-10 14:47:33 +0200

    Update shell.c

--- kernel/shell.c
+++ kernel/shell.c
@@ -7,6 +7,34 @@ void shell_omp(){
     kprint("profanOS-> ");
 }
 
+void shell_help(char suffix[]) {
+    char help[] = "HELP  - show this help\nCLEAR - clear the screen\nEND   - shutdown the system\nECHO  - print the arguments";
+
+    if (strcmp(suffix, "HELP") == 0) {
+            ckprint(help, c_magenta);
+            kprint("\n");
+    } else {
+        char tmp[strlen(suffix) + 1];
+        char rest[100];
+        char bk[100];
+        strcpy(tmp, help);
+        strcpy(rest, help);
+        for (int i = 0; i < 4; i++) {       // 4 is the number of
+            str_start_split(tmp, '\n');     // commands in the help
+            str_end_split(rest, '\n');
+            strcpy(bk, tmp);
+            str_start_split(tmp, ' ');
+            if (strcmp(tmp, suffix) == 0) {
+                ckprint(bk, c_magenta);
+                kprint("\n");
+                return;
+            }
+            strcpy(tmp, rest);
+        }
+        ckprint("command not found\n", c_red);
+    }
+}
+
 void shell_command(char *command) {
     char prefix[strlen(command)], suffix[strlen(command)];
     strcpy(prefix, command);
@@ -25,6 +53,10 @@ void shell_command(char *command) {
 
     else if (strcmp(prefix, "CLEAR") == 0) {
         clear_screen();
+    }
+
+    else if (strcmp(prefix, "HELP") == 0) {
+        shell_help(suffix);
     }
 
     else if (strcmp(prefix, "") != 0) {
