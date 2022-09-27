5073ef7102d1d1978c386d87124f9836d60d31b4
pf4 <74263128+elydre@users.noreply.github.com>
2022-09-27 10:19:36 +0200

    Update addf.h

--- zapps/addf.h
+++ zapps/addf.h
@@ -4,8 +4,11 @@
 
 #define INIT_AF(addr) int (*get_func)(int id) = (int (*)(int)) addr
 
+#define AF_fs_read_file() void (*fs_read_file)(char path[], uint32_t data[]) = (void (*)(char[], uint32_t[])) get_func(4)
+#define AF_fs_declare_read_array() void * (*fs_declare_read_array)(char path[]) = (void * (*)(char[])) get_func(8)
 #define AF_int_to_ascii() void (*int_to_ascii)(int n, char str[]) = (void (*)(int, char *)) get_func(23)
 #define AF_append() void (*append)(char *str, char c) = (void (*)(char *, char)) get_func(29)
+#define AF_mskprint() void (*mskprint)(int nb_args, ...) = (void (*)(int, ...)) get_func(37)
 #define AF_rainbow_print() void (*rainbow_print)(char msg[]) = (void (*)(char *)) get_func(39)
 #define AF_ms_sleep() void (*ms_sleep)(int) = (void (*)(int)) get_func(44)
 #define AF_sleep() void (*sleep)(int) = (void (*)(int)) get_func(43)
