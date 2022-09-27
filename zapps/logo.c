cf2ac1a822d40bb8f77ae59f04ec39826b446346
pf4 <74263128+elydre@users.noreply.github.com>
2022-09-27 10:19:28 +0200

    bin/logo

---
+++ zapps/logo.c
@@ -0,0 +1,25 @@
+#include <stdint.h>
+#include "addf.h"
+
+
+int start(int addr, int arg) {
+    INIT_AF(addr);
+
+    AF_fs_declare_read_array();
+    AF_fs_read_file();
+    AF_mskprint();
+
+    char path[] = "/user/logo.txt";
+    uint32_t * data = fs_declare_read_array(path);
+    char * str = fs_declare_read_array(path);
+    
+    fs_read_file(path, data);
+
+    int i;
+    for (i = 0; data[i] != (uint32_t) -1; i++)
+        str[i] = (char) data[i];
+    str[i] = '\n';
+
+    mskprint(1, str);
+    return arg;
+}
