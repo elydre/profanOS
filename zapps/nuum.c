45176ca134cd57deff63d2219cefe7c162e66abe
pf4 <74263128+elydre@users.noreply.github.com>
2022-10-11 11:45:45 +0200

    Update nuum.c

--- zapps/nuum.c
+++ zapps/nuum.c
@@ -55,30 +55,4 @@ float get_distance(float x1, float y1, float x2, float y2) {
     return sqrt(dx * dx + dy * dy);
 }
 
-void calc_slice_height(int * slice_height, int width, float player_x, float player_y, float rot) {
-    int i;
-    float angle;
-    float distance;
-    float x, y;
-    int map_x, map_y;
-    int wall_height;
-
-    for (i = 0; i < width; i++) {
-        angle = rot - fov / 2 + fov * i / width;
-        x = player_x;
-        y = player_y;
-        distance = 0;
-        while (1) {
-            x += player_speed * cos(angle);
-            y += player_speed * sin(angle);
-            distance += player_speed;
-            map_x = (int)x;
-            map_y = (int)y;
-            if (map[map_x + map_y * mapsize] == 1) {
-                break;
-            }
-        }
-        wall_height = (int)(height / distance);
-        slice_height[i] = wall_height;
-    }
-}
+void calc_slice_height(int * slice_height, int width, float player_x, float player_y, float rot);
