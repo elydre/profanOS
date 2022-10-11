561676de4df7b37c596cf3593b8cbbe471953726
pf4 <74263128+elydre@users.noreply.github.com>
2022-10-11 19:53:13 +0200

    Update doom.c

--- zapps/doom.c
+++ zapps/doom.c
@@ -2,31 +2,30 @@
 
 #define mapsize 10
 #define pi 3.14159
-#define calc_speed 0.1
+#define calc_speed 0.5
 
 #define height_at_1m 3
-#define player_speed 0.1
+#define player_speed 0.3
 #define rot_speed 5
-#define fov (pi * 2)
+#define fov (pi / 3)
 
-#define wall_color 4
 #define floor_color 0
 #define ceiling_color 3
 
 int MAP[] = {
-    1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
-    1, 0, 0, 0, 0, 0, 0, 0, 0, 1,
-    1, 0, 0, 0, 0, 0, 0, 0, 0, 1,
-    1, 0, 0, 0, 0, 0, 0, 0, 0, 1,
-    1, 0, 0, 0, 0, 0, 0, 0, 0, 1,
-    1, 0, 0, 0, 0, 0, 0, 0, 0, 1,
-    1, 0, 0, 0, 0, 0, 0, 0, 0, 1,
-    1, 0, 0, 0, 0, 0, 0, 0, 0, 1,
-    1, 0, 0, 0, 0, 0, 0, 0, 0, 1,
-    1, 1, 1, 1, 1, 1, 1, 1, 1, 1
+    1, 1, 1, 1, 1, 1, 1, 1, 1, 6,
+    2, 0, 0, 0, 0, 0, 0, 0, 0, 6,
+    2, 0, 0, 0, 0, 0, 0, 0, 0, 6,
+    2, 0, 0, 0, 0, 0, 0, 0, 0, 6,
+    2, 0, 0, 0, 0, 0, 0, 0, 0, 6,
+    2, 0, 0, 0, 0, 0, 0, 0, 0, 6,
+    2, 0, 0, 0, 0, 0, 0, 0, 0, 6,
+    2, 0, 0, 0, 0, 0, 0, 0, 0, 6,
+    2, 0, 0, 0, 0, 0, 0, 0, 0, 6,
+    2, 7, 7, 7, 7, 7, 7, 7, 7, 7
 };
 
-double get_distance(double x, double y, double rad_angle);
+double get_distance(double x, double y, double rad_angle, int * color);
 
 double cos(double x);
 double sin(double x);
@@ -37,7 +36,6 @@ int main(int arg) {
     // 2.5D game doom like
     double x = 5, y = 5;
     double rot = 0; // in degrees
-    char info[100];
     char temp[5];
 
 
@@ -47,12 +45,14 @@ int main(int arg) {
     int center, top, bottom;
     double part_height;
 
+    int color;
+
     c_vga_320_mode();
     while (c_kb_get_scancode() != 1) {
         for (int i = 0; i < width; i++) {
             double angle = deg_to_rad(rot + (fov / 2) - (fov * i / width));
 
-            part_height = height_at_1m / get_distance(x, y, angle);
+            part_height = height_at_1m / get_distance(x, y, angle, &color);
             center = (int) (half_height * part_height);
             top = (int) (half_height - center);
             bottom = (int) (half_height + center);
@@ -61,7 +61,7 @@ int main(int arg) {
                 c_vga_put_pixel(i, j, ceiling_color);
             }
             for (int j = top; j < bottom; j++) {
-                c_vga_put_pixel(i, j, wall_color);
+                c_vga_put_pixel(i, j, color);
             }
             for (int j = bottom; j < height; j++) {
                 c_vga_put_pixel(i, j, floor_color);
@@ -69,38 +69,41 @@ int main(int arg) {
         }
         
         if (c_kb_get_scancode() == KB_D) {
-            rot += rot_speed;
-        }
-        if (c_kb_get_scancode() == KB_Q) {
             rot -= rot_speed;
         }
+        if (c_kb_get_scancode() == KB_Q) {
+            rot += rot_speed;
+        }
         if (c_kb_get_scancode() == KB_Z) {
-            x += cos(rot) * player_speed;
-            y += sin(rot) * player_speed;
+            x += cos(deg_to_rad(rot)) * player_speed;
+            y += sin(deg_to_rad(rot)) * player_speed;
         }
         if (c_kb_get_scancode() == KB_S) {
-            x -= cos(rot) * player_speed;
-            y -= sin(rot) * player_speed;
+            x -= cos(deg_to_rad(rot)) * player_speed;
+            y -= sin(deg_to_rad(rot)) * player_speed;
         }
 
         if (rot > 360) rot -= 360;
         if (rot < 0) rot += 360;
         if (x < 1) x = 1;
         if (y < 1) y = 1;
-        if (x > mapsize - 1) x = mapsize - 1;
-        if (y > mapsize - 1) y = mapsize - 1;
+        if (x > mapsize - 2) x = mapsize - 2;
+        if (y > mapsize - 2) y = mapsize - 2;
 
-        c_str_cpy(info, "x: ");
-        c_int_to_ascii((int) x, temp);
-        c_str_cat(info, temp);
-        c_str_cat(info, " y: ");
-        c_int_to_ascii((int) y, temp);
-        c_str_cat(info, temp);
-        c_str_cat(info, " rot: ");
+        // draw minimap
+        for (int i = 0; i < mapsize * mapsize; i++) {
+            c_vga_draw_rect(width - mapsize * 2 + i % mapsize * 2, i / mapsize * 2, 2, 2, MAP[i]);
+        }
+
+        // draw player
+        c_vga_draw_rect(width - mapsize * 2 + (int) x * 2, (int) y * 2, 2, 2, 63);
+
+        // draw player direction
+        c_vga_draw_rect(width - mapsize * 2 + (int) (x + cos(deg_to_rad(rot)) * 2) * 2, (int) (y + sin(deg_to_rad(rot)) * 2) * 2, 2, 2, 61);
+
         c_int_to_ascii((int) rot, temp);
-        c_str_cat(info, temp);
-        c_vga_print(0, 0, info, 0, 0);
-        
+        c_vga_print(width - 30, height - 10, temp, 0, 63);
+
         c_ms_sleep(20);
     }
 
@@ -116,13 +119,13 @@ double modd(double x, double y) {
 double cos(double x) {
     // cos of x in radians
     x = modd(x, pi);
-    return 1 - ((x * x) / (2)) + ((x * x * x * x) / (24)) - ((x * x * x * x * x * x) / (720)) + ((x * x * x * x * x * x * x * x) / (40320)) - ((x * x * x * x * x * x * x * x * x * x) / (3628800)) + ((x * x * x * x * x * x * x * x * x * x * x * x) / (479001600));
+    return 1 - ((x * x) / (2)) + ((x * x * x * x) / (24)) - ((x * x * x * x * x * x) / (720));
 }
 
 double sin(double x) {
     // sin of x in radians
     x = modd(x, pi);
-    return x - ((x * x * x) / (6)) + ((x * x * x * x * x) / (120)) - ((x * x * x * x * x * x * x) / (5040)) + ((x * x * x * x * x * x * x * x * x) / (362880)) - ((x * x * x * x * x * x * x * x * x * x * x) / (39916800)) + ((x * x * x * x * x * x * x * x * x * x * x * x) / (6227020800));
+    return x - ((x * x * x) / (6)) + ((x * x * x * x * x) / (120)) - ((x * x * x * x * x * x * x) / (5040));
 }
 
 double max(double a, double b) {
@@ -137,11 +140,9 @@ double deg_to_rad(int x) {
     return x * pi / 180;
 }
 
-double get_distance(double x, double y, double rad_angle) {
+double get_distance(double x, double y, double rad_angle, int * color) {
     double dx = cos(rad_angle);
     double dy = sin(rad_angle);
-
-    c_fskprint("cos: %d ", (int) (dx * 100), (int) (dy * 100));
 
     double px = x;
     double py = y;
@@ -154,7 +155,8 @@ double get_distance(double x, double y, double rad_angle) {
         cell_x = (int) px;
         cell_y = (int) py;
 
-        if (MAP[cell_y * mapsize + cell_x] == 1) {
+        if (MAP[cell_y * mapsize + cell_x]) {
+            * color = MAP[cell_y * mapsize + cell_x];
             return distance;
         }
 
@@ -163,5 +165,6 @@ double get_distance(double x, double y, double rad_angle) {
         px += dx * calc_speed;
         py += dy * calc_speed;
     }
+    * color = 0;
     return distance;
 }
