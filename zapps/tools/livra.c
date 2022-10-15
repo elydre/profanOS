ea4280134f4224d924e28c146e154f78205bf25a
pf4 <74263128+elydre@users.noreply.github.com>
2022-10-15 21:06:36 +0200

    Create livra.c

---
+++ zapps/tools/livra.c
@@ -0,0 +1,95 @@
+#include "syscall.h"
+
+#define LSIZE 1024
+
+/* https://github.com/elydre/ivra *
+    end     0
+    while   1 r
+    set     2 r v
+    cp      3 r r
+    show    4 r
+    add     5 r r o
+    sub     6 r r o
+    mul     7 r r o
+    div     8 r r o
+    egal    9 r r o
+    sup     10 r r o
+    or      11 r r o
+    not     12 r
+    mod     13 r r o
+*/
+
+int arg_cont[] = {
+    0, 1, 2, 2, 1, 3, 3, 3, 3, 3, 3, 3, 1, 3
+};
+
+int * mem;
+int prog[] = {
+    2, 2, 2, 2, 3, 100, 1, 0, 5,
+    2, 0, 2, 2, 4, 1, 2, 5, 1, 1,
+    4, 5, 5, 0, 5, 13, 2, 5, 4,
+    9, 5, 2, 1, 1, 1, 4, 2, 2, 1,
+    0, 0, 0, 9, 2, 3, 0, 12, 0, 0,
+};
+
+void start_inter(int * code, int code_size, int while_id);
+
+int main(int arg) {
+    int * meml = c_calloc(LSIZE * sizeof(int));
+    mem = meml;
+
+    meml[0] = 1;
+    
+    start_inter(prog, sizeof(prog) / sizeof(int), 0);
+
+    c_free(meml);
+    return arg;
+}
+
+void start_inter(int * code, int code_size, int while_id) {
+    int to_pass, mode;
+    while (mem[while_id]) {
+        to_pass = 0;
+        for (int instruc = 0; instruc < code_size; instruc++) {
+            mode = code[instruc];
+
+            if (mode == 0) {
+                to_pass--;
+                if (to_pass < 0) {
+                    break;
+                }
+            }
+
+            else if (to_pass) {
+                if (mode == 1) {
+                    to_pass++;
+                }
+            }
+
+            else if (mode == 2) mem[code[instruc + 1]] = code[instruc + 2];
+            else if (mode == 3) mem[code[instruc + 1]] = mem[code[instruc + 2]];
+            else if (mode == 4) c_fskprint("%d\n", mem[code[instruc + 1]]);
+            else if (mode == 5) mem[code[instruc + 3]] = mem[code[instruc + 1]] + mem[code[instruc + 2]];
+            else if (mode == 6) mem[code[instruc + 3]] = mem[code[instruc + 1]] - mem[code[instruc + 2]];
+            else if (mode == 7) mem[code[instruc + 3]] = mem[code[instruc + 1]] * mem[code[instruc + 2]];
+            else if (mode == 8) mem[code[instruc + 3]] = mem[code[instruc + 1]] / mem[code[instruc + 2]];
+            else if (mode == 9) mem[code[instruc + 3]] = mem[code[instruc + 1]] == mem[code[instruc + 2]];
+            else if (mode == 10) mem[code[instruc + 3]] = mem[code[instruc + 1]] > mem[code[instruc + 2]];
+            else if (mode == 11) mem[code[instruc + 3]] = mem[code[instruc + 1]] || mem[code[instruc + 2]];
+            else if (mode == 12) mem[code[instruc + 1]] = !mem[code[instruc + 1]];
+            else if (mode == 13) mem[code[instruc + 3]] = mem[code[instruc + 1]] % mem[code[instruc + 2]];
+
+            instruc += arg_cont[mode];
+
+            if (mode == 1 && to_pass == 0) {
+                int * new_code = c_malloc((code_size - instruc - 1) * sizeof(int));
+                for (int i = 0; i < code_size - instruc - 1; i++) {
+                    new_code[i] = code[instruc + i + 1];
+                }
+                start_inter(new_code, code_size - instruc - 1, code[instruc]);
+                c_free(new_code);
+                to_pass++;
+            }
+        }
+    }
+}
