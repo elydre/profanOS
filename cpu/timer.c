e7695077eca216ee673f228a4c9559f407341933
pf4 <74263128+elydre@users.noreply.github.com>
2022-10-17 00:10:13 +0200

    update

--- cpu/timer.c
+++ cpu/timer.c
@@ -10,9 +10,9 @@ uint32_t tick = 0;
 static void timer_callback(registers_t *regs) {
     UNUSED(regs);
     tick++;
-    if (tick % 1000 == 0) {
+    /*if (tick % 1000 == 0) {
         yield(1);
-    }
+    }*/
 }
 
 uint32_t timer_get_tick() {
--- kernel/kernel.c
+++ kernel/kernel.c
@@ -7,6 +7,21 @@
 #include <time.h>
 #include <task.h>
 
+int find_mem_end() {
+    // Find the end of the memory
+    // TODO: move in mem.c or system.c
+    int *addr_min = (int *) 0x200000;
+    int *addr_max = (int *) 0x40000000;
+    int *addr_test;
+    while (addr_max - addr_min > 1) {
+        addr_test = addr_min + (addr_max - addr_min) / 2;
+        * addr_test = 0x1234;
+        if (*addr_test == 0x1234) addr_min = addr_test;
+        else addr_max = addr_test;
+    }
+    return (int) addr_max;
+}
+
 void kernel_main() {
     clear_screen();
     fskprint("$6booting profanOS...\n");
@@ -31,6 +46,8 @@ void kernel_main() {
 
     rainbow_print("\n\nWelcome to profanOS!\n");
     fskprint("$C~~ version $4%s $C~~\n\n", VERSION);
+
+    fskprint("Memory end: %dMo\n", find_mem_end() / 1024 / 1024);
 
     // launch of the shell.bin
     sys_run_ifexist("/bin/shell.bin", 0);
