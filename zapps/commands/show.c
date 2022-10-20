de40d8e0d316b5b219bf359177d7b895641eb493
pf4 <74263128+elydre@users.noreply.github.com>
2022-10-20 20:13:32 +0200

    0.7.2 - shell rework

--- zapps/commands/show.c
+++ zapps/commands/show.c
@@ -10,15 +10,19 @@ int main(int argc, char **argv) {
     if(!(c_str_count(suffix, '.'))) c_str_cat(suffix, ".img");
     char *file = c_malloc(c_str_len(suffix) + c_str_len(current_dir) + 2);
     assemble_path(current_dir, suffix, file);
-    c_vga_320_mode();
-    Sprite_t sprite = c_lib2d_init_sprite(file);
-    c_lib2d_print_sprite(0, 0, sprite);
-    while (c_kb_get_scancode() != 1);
-    c_lib2d_free_sprite(sprite);
-    c_vga_text_mode();
-    c_free(file);
-    c_free(suffix);
+
+    if (c_fs_does_path_exists(file) && c_fs_type_sector(c_fs_path_to_id(file, 0)) == 2) {
+        c_vga_320_mode();
+        Sprite_t sprite = c_lib2d_init_sprite(file);
+        c_lib2d_print_sprite(0, 0, sprite);
+        while (c_kb_get_scancode() != 1);
+        c_lib2d_free_sprite(sprite);
+        c_vga_text_mode();
+    } else c_fskprint("$3%s$B file not found\n", file);
+    
     c_free(current_dir);
+    c_free(suffix);
+    c_free(file);
     return 0;
 }
 
