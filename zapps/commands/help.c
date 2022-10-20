7409fceb7c632a63f036dcf44a58627800c09e9c
pf4 <74263128+elydre@users.noreply.github.com>
2022-10-20 21:30:42 +0200

    Update help.c

--- zapps/commands/help.c
+++ zapps/commands/help.c
@@ -1,12 +1,6 @@
 #include <syscall.h>
 
-void assemble_path(char old[], char new[], char result[]);
-
 int main(int argc, char **argv) {
-    if (argc > 2) {
-        c_fskprint("$BSpecialised help is not yet implemented.\n");
-        return 0;
-    }
     char *file = "/zada/shell_help.txt";
     if (c_fs_does_path_exists(file) && c_fs_type_sector(c_fs_path_to_id(file, 0)) == 2) {
         uint32_t * file_content = c_fs_declare_read_array(file);
@@ -19,7 +13,7 @@ int main(int argc, char **argv) {
         c_ckprint(char_content, c_magenta);
         c_free(file_content);
         c_free(char_content);
-    } else c_fskprint("$Bcommand not found\n");
+    } else c_fskprint("$Bshell_help.txt not found\n", file);
     c_free(file);
     return 0;
 }
