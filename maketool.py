89e75f4b7605a04ef3556795f43415c9afb59134
pf4 <74263128+elydre@users.noreply.github.com>
2022-10-25 11:48:11 +0200

    Update maketool.py

--- maketool.py
+++ maketool.py
@@ -7,7 +7,7 @@ import sys, os
 SRC_DIRECTORY = ["boot", "kernel", "drivers", "cpu", "libc"]
 
 INCLUDE_DIR = "include"
-INCLUDE_SUB = [".", "driver", "cpu", "gui"]
+INCLUDE_SUB = [".", "driver", "cpu", "gui", "libc"]
 
 ZAPPS_DIR = "zapps"
 
