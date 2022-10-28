31343bdfcf0636186b74ded1fcd8a609d7f855f5
pf4 <74263128+elydre@users.noreply.github.com>
2022-10-28 11:02:52 +0200

    0.7.7 - ramdisk lim

--- include/libc/ramdisk.h
+++ include/libc/ramdisk.h
@@ -8,5 +8,6 @@ void ramdisk_read_sector(uint32_t LBA, uint32_t out[]);
 void ramdisk_write_sector(uint32_t LBA, uint32_t bytes[]);
 
 uint32_t get_ramdisk_address();
+uint32_t get_ramdisk_size();
 
 #endif
