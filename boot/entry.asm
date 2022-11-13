2eae73bc4adceda9d44b942d05a80788b73d3c68
pf4 <74263128+elydre@users.noreply.github.com>
2022-11-13 13:29:57 +0100

    boot

--- boot/entry.asm
+++ boot/entry.asm
@@ -3,17 +3,17 @@
 [extern kernel_main]
 [global loader]
 
-MAGIC_NUMBER equ 0x1BADB002         ; define the magic number constant
-FLAGS        equ 0x0                ; multiboot flags
-CHECKSUM     equ -MAGIC_NUMBER      ; calculate the checksum
-                                    ; (magic number + checksum + flags should equal 0)
 KERNEL_STACK_SIZE equ 4096          ; size of stack in bytes
 
-section .multiboot                  ; start of the multiboot header
-align 4                             ; align at 4 bytes
-    dd MAGIC_NUMBER                 ; write the magic number to the machine code,
-    dd FLAGS                        ; the flags,
-    dd CHECKSUM                     ; and the checksum
+FLAGS equ 0b111
+MAGIC equ 0x1BADB002
+CHECKSUM equ -(MAGIC + FLAGS)
+
+section .multiboot
+align 4
+    dd MAGIC
+    dd FLAGS
+    dd CHECKSUM
 
 section .text                       ; start of the text (code) section
 align 4                             ; the code must be 4 byte aligned
