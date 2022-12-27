d84723faee3b1dec51cc883ba21d137b88df6805
Loris <monestierloris11@gmail.com>
2022-12-27 05:04:04 +0100

    stdbool.h header for zlibs

---
+++ include/zlibs/stdbool.h
@@ -0,0 +1,10 @@
+#ifndef STDBOOL_ID
+#define STDBOOL_ID 
+// just the header, it has no functions
+
+#define bool _Bool
+#define true 1
+#define false 0
+#define __bool_true_false_are_defined 1
+
+#endif
