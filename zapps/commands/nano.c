40e5830a909385b656337234a54a06149ec4ccf3
Loris <monestierloris11@gmail.com>
2022-12-27 11:58:41 +0100

    nano.c fix

--- zapps/commands/nano.c
+++ zapps/commands/nano.c
@@ -3,28 +3,27 @@
 #include <stdio.h>
 #include <syscall.h>
 
+char **text_buffer = NULL;
+
 int main(int argc, char **argv) {
-    printf("argc = %d\n", argc);
-    for (int i = 0; i < argc; i++) {
-        printf("argv[%d] = %s\n", i, argv[i]);
-    }
+
+    text_buffer = malloc(1 * sizeof(char*)); // we will realloc later
+    text_buffer[0] = calloc(1, sizeof(char)); // we will realloc later
+
     if (argc == 2) {
         // we open a new file that will be saved later
     }
     else {
         // we open the file that will be edited
-        char *filename = calloc(strlen(argv[1]) + 1 + strlen(argv[2]), sizeof(char));
+        char *filename = calloc(strlen(argv[1]) + 1 + strlen(argv[2]) + 1, sizeof(char));
         strcpy(filename, argv[1]);
         strcat(filename, "/");
         strcat(filename, argv[2]);
-        strcat(filename, "\0");
-        printf("Opening file %s\n", filename);
         FILE *file = fopen(filename, "r+");
         if (file == NULL) {
             printf("File %s does not exist\n", filename);
             return 1;
         }
         free(filename);
-        printf("Editing file %s\n", file->name);
     }
 }
