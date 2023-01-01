af0dfb6b3137bae7528280dd2585e6b81ab701f9
Loris <monestierloris11@gmail.com>
2023-01-01 20:27:40 +0100

    delete_dir

--- kpart/filesystem.c
+++ kpart/filesystem.c
@@ -395,7 +395,7 @@ uint32_t i_path_to_id(char *path, char *current_path, uint32_t sector) {
 }
 
 void i_delete_dir(uint32_t id) {
-    sys_warning("Not implemented yet : delete_file");
+    sys_warning("Not implemented yet : delete_dir");
     (void) id;
 }
 
