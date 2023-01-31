97fe0a206a29b58bf130f2a832b70705a23da516
pf4 <74263128+elydre@users.noreply.github.com>
2023-01-31 17:04:36 +0100

    0.10.7b - setjmp zlib

--- include/kernel/system.h
+++ include/kernel/system.h
@@ -3,7 +3,7 @@
 
 // build settings
 
-#define KERNEL_VERSION  "0.10.7"
+#define KERNEL_VERSION  "0.10.7b"
 
 #define PROCESS_MAX     20
 #define RAMDISK_SECTOR  4096
--- kernel/dily.c
+++ kernel/dily.c
@@ -26,6 +26,7 @@ lib_t libs_at_boot[] = {
     {1010, "/lib/i_mouse.bin"},
     {1011, "/lib/math.bin"},
     {1012, "/lib/time.bin"},
+    {1013, "/lib/setjmp.bin"},
 };
 
 
