2596e75e915c8ac61e72468dd5f888398735f300
pf4 <74263128+elydre@users.noreply.github.com>
2023-02-19 14:25:51 +0100

    Update i_iolib.c

--- zlibs/i_iolib.c
+++ zlibs/i_iolib.c
@@ -9,7 +9,8 @@
 #include <stdarg.h>
 
 // input() setings
-#define FIRST_L 8
+#define SLEEP_T 30
+#define FIRST_L 12
 
 // keyboard scancodes
 #define SC_MAX 57
@@ -23,6 +24,7 @@
 #define BACKSPACE 14
 #define DEL 83
 #define ENTER 28
+#define RESEND 224
 
 // private functions
 
@@ -176,11 +178,11 @@ void rainbow_print(char message[]) {
 
 void input_wh(char out_buffer[], int size, char color, char ** history, int history_size) {
     int old_cursor = c_get_cursor_offset();
+    int sc, last_sc, last_sc_sgt = 0;
     int buffer_actual_size = 0;
     int history_index = 0;
     int buffer_index = 0;
     int key_ticks = 0;
-    int sc, last_sc;
     int shift = 0;
     int new_pos;
 
@@ -188,22 +190,30 @@ void input_wh(char out_buffer[], int size, char color, char ** history, int hist
     int col = c_gt_get_max_cols();
 
     clean_buffer(out_buffer, size);
-
+    
     do {
         sc = c_kb_get_scfh();
     } while (sc == ENTER);
 
     c_kb_reset_history();
+
     c_cursor_blink(1);
 
     while (sc != ENTER) {
-        ms_sleep(10);
+        ms_sleep(SLEEP_T);
 
-        last_sc = sc;
         sc = c_kb_get_scfh();
-        
+
+        if (sc == RESEND || sc == 0) {
+            sc = last_sc_sgt;
+        } else {
+            last_sc_sgt = sc;
+        }
+
+        if (!sc) continue;
         if (sc != last_sc) key_ticks = 0;
         else key_ticks++;
+        last_sc = sc;
 
         if (key_ticks < FIRST_L && key_ticks) continue;
 
@@ -291,7 +301,6 @@ void input_wh(char out_buffer[], int size, char color, char ** history, int hist
             buffer_actual_size++;
             buffer_index++;
         }
-
 
         c_set_cursor_offset(old_cursor);
         c_ckprint(out_buffer, color);
