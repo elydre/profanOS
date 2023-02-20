6dab62f1dd182cc3c1efadac50e5bc1048e52424
pf4 <74263128+elydre@users.noreply.github.com>
2023-02-20 10:56:21 +0100

    rewrite commands

--- zapps/commands/cat.c
+++ zapps/commands/cat.c
@@ -1,36 +1,32 @@
 #include <syscall.h>
+#include <profan.h>
 #include <stdlib.h>
 #include <string.h>
 #include <stdio.h>
 
-void assemble_path(char old[], char new[], char result[]);;
 
 int main(int argc, char **argv) {
-    char *fpath = malloc(256);
-    char *suffix = malloc(256);
-    strcpy(fpath, argv[1]);
-    strcpy(suffix, argv[2]);
-    char *file = malloc(strlen(suffix) + strlen(fpath) + 2);
-    assemble_path(fpath, suffix, file);
+    if (argc < 3) {
+        printf("$BUsage: cat <file>\n");
+        return 0;
+    }
+    
+    char *file = malloc(strlen(argv[1]) + strlen(argv[2]) + 2);
+    assemble_path(argv[1], argv[2], file);
+
     if (c_fs_does_path_exists(file) && c_fs_get_sector_type(c_fs_path_to_id(file)) == 2) {
         char *char_content = c_fs_declare_read_array(file);
         c_fs_read_file(file, (uint8_t *) char_content);
-        c_ckprint(char_content, c_magenta);
-        printf("\n");
-        free(char_content);
-    } else printf("$3%s$B file not found\n", file);
-    free(file);
-    free(fpath);
-    free(suffix);
-    return 0;
-}
 
-void assemble_path(char old[], char new[], char result[]) {
-    result[0] = '\0'; strcpy(result, old);
-    if (result[strlen(result) - 1] != '/') {
-        strncat(result, "/", 1);
+        c_ckprint(char_content, c_magenta);
+        c_kprint("\n");
+
+        free(char_content);
+    } else {
+        printf("$3%s$B file not found\n", file);
     }
-    for (unsigned int i = 0; i < strlen(new); i++) {
-        strncat(result, &new[i], 1);
-    }
+
+    free(file);
+
+    return 0;
 }
--- zapps/commands/mkdir.c
+++ zapps/commands/mkdir.c
@@ -1,19 +1,18 @@
 #include <syscall.h>
 #include <string.h>
 #include <stdio.h>
-#include <stdlib.h>
 
 int main(int argc, char **argv) {
-    char *suffix = malloc(256);
-    strcpy(suffix, argv[2]);
-    char *current_dir = malloc(256);
-    strcpy(current_dir, argv[1]);
-    if (!strcmp(suffix, "..")) {
+    if (argc < 3) {
+        printf("$BUsage: mkdir <name>\n");
+        return 0;
+    }
+
+    if (!strcmp(argv[2], "..")) {
         printf("$3Un dossier ne peut pas avoir comme nom .. !\n");
     } else {
-        c_fs_make_dir(current_dir, suffix);
+        c_fs_make_dir(argv[1], argv[2]);
     }
-    free(current_dir);
-    free(suffix);
+
     return 0;
 }
