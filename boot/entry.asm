b4bc6f8c123060420547fa167a7ceed6e14914a0
pf4 <74263128+elydre@users.noreply.github.com>
2023-02-28 11:28:58 +0100

    commit très important
    Co-Authored-By: Loris <74245537+Lorisredstone@users.noreply.github.com>

--- boot/entry.asm
+++ boot/entry.asm
@@ -3,8 +3,8 @@
 [extern kernel_main]
 [global loader]
 
-FLAGS equ 0b111
-MAGIC equ 0x1BADB002
+FLAGS equ 0b111 
+MAGIC equ 0x1BADB002        ; it's wrong
 CHECKSUM equ -(MAGIC + FLAGS)
 
 section .multiboot
@@ -14,7 +14,7 @@ align 4
     dd CHECKSUM
 
 section .text               ; start of the text (code) section
-align 4
+align 4                     ; TODO : change this to align 8
 loader:
     push ebx                ; keep the multiboot info pointer
     cli                     ; disable interrupts
