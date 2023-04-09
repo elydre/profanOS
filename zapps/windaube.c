ca3f031f9e945c953b95765a519563adecb773c8
pf4 <74263128+elydre@users.noreply.github.com>
2023-04-09 15:09:17 +0200

    push

--- zapps/windaube.c
+++ zapps/windaube.c
@@ -21,7 +21,10 @@ int main(int argc, char **argv) {
     c_run_ifexist("/bin/winapps/counter.bin", 0, NULL);
     c_run_ifexist("/bin/winapps/usage.bin", 0, NULL);
     c_run_ifexist("/bin/winapps/term.bin", 0, NULL);
-    // c_run_ifexist("/bin/winapps/cube.bin", 0, NULL);
+    c_run_ifexist("/bin/winapps/cube.bin", 0, NULL);
+
+    c_run_ifexist("/bin/winapps/test1.bin", 0, NULL);
+    c_run_ifexist("/bin/winapps/test2.bin", 0, NULL);
 
     while (1) {
         desktop_run_stack(desktop);
