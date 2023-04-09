614e2095136958e5a6c24650d9f0ef1432e229e1
pf4 <74263128+elydre@users.noreply.github.com>
2023-04-09 15:17:50 +0200

    mhh

--- zapps/winapps/test1.c
+++ zapps/winapps/test1.c
@@ -4,6 +4,8 @@
 
 #include <syscall.h>
 #include <stdlib.h>
+#include <stdio.h>
+
 
 int is_running;
 
@@ -15,7 +17,7 @@ int main(int argc, char **argv) {
     // wake up the parent process
     c_process_wakeup(c_process_get_ppid(c_process_get_pid()));
 
-    // get the main desktop
+    /*// get the main desktop
     desktop_t *main_desktop = desktop_get_main();
 
     // create a window and add an exit button
@@ -23,23 +25,25 @@ int main(int argc, char **argv) {
     wadds_create_exitbt(window, exit_callback);
     desktop_refresh(main_desktop);
 
-    // set the window background to black
+    // set the window background to black*/
 
     is_running = 1;
     while (is_running) {
-        window_fill(window, 0xFF0000);
+        /*window_fill(window, 0xFF0000);
         window_refresh(window);
 
         window_fill(window, 0x00FF00);
         window_refresh(window);
 
         window_fill(window, 0x0000FF);
-        window_refresh(window);
+        window_refresh(window);*/
+        // ms_sleep(100);
+        c_kprint("Hello World from test1.c\n");
     }
 
-    // destroy window and wait for it to be deleted
+    /*// destroy window and wait for it to be deleted
     window_delete(window);
-    window_wait_delete(main_desktop, window);
+    window_wait_delete(main_desktop, window);*/
 
     return 0;
 }
--- zapps/windaube.c
+++ zapps/windaube.c
@@ -3,6 +3,7 @@
 #include <i_vgui.h>
 
 #include <syscall.h>
+#include <stdio.h>
 
 #define MAX_WINDOWS 10
 
@@ -10,26 +11,38 @@
 #define SCREEN_HEIGHT 768
 
 int main(int argc, char **argv) {
-    vgui_t vgui = vgui_setup(SCREEN_WIDTH, SCREEN_HEIGHT);
+    //vgui_t vgui = vgui_setup(SCREEN_WIDTH, SCREEN_HEIGHT);
+    //desktop_t *desktop = desktop_init(&vgui, MAX_WINDOWS, SCREEN_WIDTH, SCREEN_HEIGHT);
+    //desktop_refresh(desktop);
 
-    desktop_t *desktop = desktop_init(&vgui, MAX_WINDOWS, SCREEN_WIDTH, SCREEN_HEIGHT);
-    desktop_refresh(desktop);
-
-    c_run_ifexist("/bin/winapps/cpu.bin", 0, NULL);
-    c_run_ifexist("/bin/winapps/demo.bin", 0, NULL);
-    c_run_ifexist("/bin/winapps/pong.bin", 0, NULL);
-    c_run_ifexist("/bin/winapps/counter.bin", 0, NULL);
-    c_run_ifexist("/bin/winapps/usage.bin", 0, NULL);
-    c_run_ifexist("/bin/winapps/term.bin", 0, NULL);
-    c_run_ifexist("/bin/winapps/cube.bin", 0, NULL);
+    // c_run_ifexist("/bin/winapps/cpu.bin", 0, NULL);
+    // c_run_ifexist("/bin/winapps/demo.bin", 0, NULL);
+    // c_run_ifexist("/bin/winapps/pong.bin", 0, NULL);
+    // c_run_ifexist("/bin/winapps/counter.bin", 0, NULL);
+    // c_run_ifexist("/bin/winapps/usage.bin", 0, NULL);
+    // c_run_ifexist("/bin/winapps/term.bin", 0, NULL);
+    // c_run_ifexist("/bin/winapps/cube.bin", 0, NULL);
 
     c_run_ifexist("/bin/winapps/test1.bin", 0, NULL);
-    c_run_ifexist("/bin/winapps/test2.bin", 0, NULL);
+    c_run_ifexist("/bin/winapps/test1.bin", 0, NULL);
+    c_run_ifexist("/bin/winapps/test1.bin", 0, NULL);
+    c_run_ifexist("/bin/winapps/test1.bin", 0, NULL);
+    c_run_ifexist("/bin/winapps/test1.bin", 0, NULL);
+    c_run_ifexist("/bin/winapps/test1.bin", 0, NULL);
+    c_run_ifexist("/bin/winapps/test1.bin", 0, NULL);
+    c_run_ifexist("/bin/winapps/test1.bin", 0, NULL);
+    c_run_ifexist("/bin/winapps/test1.bin", 0, NULL);
+    c_run_ifexist("/bin/winapps/test1.bin", 0, NULL);
+    c_run_ifexist("/bin/winapps/test1.bin", 0, NULL);
+    c_run_ifexist("/bin/winapps/test1.bin", 0, NULL);
+    c_run_ifexist("/bin/winapps/test1.bin", 0, NULL);
+    // c_run_ifexist("/bin/winapps/test2.bin", 0, NULL);
 
     while (1) {
-        desktop_run_stack(desktop);
-        refresh_mouse(desktop);
-        ms_sleep(10);
+        // desktop_run_stack(desktop);
+        // refresh_mouse(desktop);
+        // ms_sleep(10);
+        c_kprint("Hello World from windaube\n");
     }
 
     return 0;
