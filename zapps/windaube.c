06b2c943ca69d7a53757dc91e3036a0ca6dd6619
pf4 <74263128+elydre@users.noreply.github.com>
2023-04-09 15:51:55 +0200

    Revert "mhh"
    This reverts commit 614e2095136958e5a6c24650d9f0ef1432e229e1.

--- zapps/windaube.c
+++ zapps/windaube.c
@@ -3,7 +3,6 @@
 #include <i_vgui.h>
 
 #include <syscall.h>
-#include <stdio.h>
 
 #define MAX_WINDOWS 10
 
@@ -11,38 +10,26 @@
 #define SCREEN_HEIGHT 768
 
 int main(int argc, char **argv) {
-    //vgui_t vgui = vgui_setup(SCREEN_WIDTH, SCREEN_HEIGHT);
-    //desktop_t *desktop = desktop_init(&vgui, MAX_WINDOWS, SCREEN_WIDTH, SCREEN_HEIGHT);
-    //desktop_refresh(desktop);
+    vgui_t vgui = vgui_setup(SCREEN_WIDTH, SCREEN_HEIGHT);
 
-    // c_run_ifexist("/bin/winapps/cpu.bin", 0, NULL);
-    // c_run_ifexist("/bin/winapps/demo.bin", 0, NULL);
-    // c_run_ifexist("/bin/winapps/pong.bin", 0, NULL);
-    // c_run_ifexist("/bin/winapps/counter.bin", 0, NULL);
-    // c_run_ifexist("/bin/winapps/usage.bin", 0, NULL);
-    // c_run_ifexist("/bin/winapps/term.bin", 0, NULL);
-    // c_run_ifexist("/bin/winapps/cube.bin", 0, NULL);
+    desktop_t *desktop = desktop_init(&vgui, MAX_WINDOWS, SCREEN_WIDTH, SCREEN_HEIGHT);
+    desktop_refresh(desktop);
+
+    c_run_ifexist("/bin/winapps/cpu.bin", 0, NULL);
+    c_run_ifexist("/bin/winapps/demo.bin", 0, NULL);
+    c_run_ifexist("/bin/winapps/pong.bin", 0, NULL);
+    c_run_ifexist("/bin/winapps/counter.bin", 0, NULL);
+    c_run_ifexist("/bin/winapps/usage.bin", 0, NULL);
+    c_run_ifexist("/bin/winapps/term.bin", 0, NULL);
+    c_run_ifexist("/bin/winapps/cube.bin", 0, NULL);
 
     c_run_ifexist("/bin/winapps/test1.bin", 0, NULL);
-    c_run_ifexist("/bin/winapps/test1.bin", 0, NULL);
-    c_run_ifexist("/bin/winapps/test1.bin", 0, NULL);
-    c_run_ifexist("/bin/winapps/test1.bin", 0, NULL);
-    c_run_ifexist("/bin/winapps/test1.bin", 0, NULL);
-    c_run_ifexist("/bin/winapps/test1.bin", 0, NULL);
-    c_run_ifexist("/bin/winapps/test1.bin", 0, NULL);
-    c_run_ifexist("/bin/winapps/test1.bin", 0, NULL);
-    c_run_ifexist("/bin/winapps/test1.bin", 0, NULL);
-    c_run_ifexist("/bin/winapps/test1.bin", 0, NULL);
-    c_run_ifexist("/bin/winapps/test1.bin", 0, NULL);
-    c_run_ifexist("/bin/winapps/test1.bin", 0, NULL);
-    c_run_ifexist("/bin/winapps/test1.bin", 0, NULL);
-    // c_run_ifexist("/bin/winapps/test2.bin", 0, NULL);
+    c_run_ifexist("/bin/winapps/test2.bin", 0, NULL);
 
     while (1) {
-        // desktop_run_stack(desktop);
-        // refresh_mouse(desktop);
-        // ms_sleep(10);
-        c_kprint("Hello World from windaube\n");
+        desktop_run_stack(desktop);
+        refresh_mouse(desktop);
+        ms_sleep(10);
     }
 
     return 0;
