1b788d8cd3bc3856cfea5f27147c1efd8ffd64b3
pf4 <74263128+elydre@users.noreply.github.com>
2023-04-16 12:07:09 +0200

    strstr + lua

---
+++ sys_dir/user/demo.lua
@@ -0,0 +1,3 @@
+calls = require("calls")
+
+calls.serial_print("Hello, world!\n")
--- zlibs/string.c
+++ zlibs/string.c
@@ -504,8 +504,33 @@ size_t strspn(const Wchar *s1, const Wchar *s2) {
     return 0;
 }
 
-Wchar *strstr(const Wchar *s1, const Wchar *s2) {
-    fsprint("Wstrstr not implemented yet, WHY DO YOU USE IT ?\n");
+char *strstr(register char *string, char *substring) {
+    register char *a, *b;
+
+    /* First scan quickly through the two strings looking for a
+     * single-character match.  When it's found, then compare the
+     * rest of the substring.
+     */
+
+    b = substring;
+    if (*b == 0) {
+        return string;
+    }
+    for ( ; *string != 0; string += 1) {
+        if (*string != *b) {
+            continue;
+        }
+        a = string;
+        while (1) {
+            if (*b == 0) {
+                return string;
+            }
+            if (*a++ != *b++) {
+                break;
+            }
+        }
+        b = substring;
+    }
     return NULL;
 }
 
