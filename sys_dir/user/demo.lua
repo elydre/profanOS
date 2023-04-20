d29d7fe802d8a61c2b353d228aa93c286b699821
pf4 <74263128+elydre@users.noreply.github.com>
2023-04-20 15:27:45 +0200

    Update demo.lua

--- sys_dir/user/demo.lua
+++ sys_dir/user/demo.lua
@@ -1,19 +1,40 @@
-f = require("files")
+-- Définition de la résolution de l'image
+local width = 800
+local height = 600
 
-path = "/zada/"
+-- Définition des propriétés de l'ensemble de Mandelbrot
+local maxIterations = 256
+local minReal = -2
+local maxReal = 1
+local minImag = -1
+local maxImag = 1
 
-for k, e in ipairs(f.get_dir_content(path)) do
-    print("key:", k,
-          "name:", e,
-          "path:", path .. e,
-          "cnt:", f.get_dir_size(path .. e),
-          "size:", f.get_file_size(path .. e),
-          "type:", f.get_path_type(path .. e)
-    )
+-- Fonction pour convertir les coordonnées d'écran en coordonnées complexes
+local function screenToComplex(x, y)
+    local real = minReal + (maxReal - minReal) * (x / width)
+    local imag = minImag + (maxImag - minImag) * (y / height)
+    return real, imag
 end
 
-print("create:", f.create_dir("/test"))
-print("create:", f.create_file("/test/mhh.txt"))
+-- Fonction pour vérifier si un point est à l'intérieur de l'ensemble de Mandelbrot
+local function isInsideMandelbrot(real, imag, maxIterations)
+    local zReal, zImag = 0, 0
+    for i = 1, maxIterations do
+        local zReal2, zImag2 = zReal * zReal, zImag * zImag
+        zImag, zReal = 2 * zReal * zImag + imag, zReal2 - zImag2 + real
+        if zReal2 + zImag2 > 4 then
+            return false, i
+        end
+    end
+    return true, maxIterations
+end
 
--- print("set: ", f.set_file_content("/user/README.txt", "Hello World!"))
--- print("get: ", f.get_file_content("/user/README.txt"))
+-- Parcours de tous les pixels de l'image
+for y = 0, height - 1 do
+    for x = 0, width - 1 do
+        local real, imag = screenToComplex(x, y)
+        local isInside, iterations = isInsideMandelbrot(real, imag, maxIterations)
+        local color = isInside and 0x000000 or 0xFFFFFF
+        profan.setpixel(x, y, color)
+    end
+end
