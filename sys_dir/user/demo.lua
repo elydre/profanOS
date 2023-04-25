5cd71f911d25568c09b790fcd71a261da040df9d
pf4 <74263128+elydre@users.noreply.github.com>
2023-04-25 19:07:29 +0200

    Update demo.lua

--- sys_dir/user/demo.lua
+++ sys_dir/user/demo.lua
@@ -1,40 +1,42 @@
--- Définition de la résolution de l'image
+-- Definition of image resolution
 local width = 800
 local height = 600
 
--- Définition des propriétés de l'ensemble de Mandelbrot
+-- Definition of properties of the Mandelbrot set
 local maxIterations = 256
 local minReal = -2
 local maxReal = 1
 local minImag = -1
 local maxImag = 1
 
--- Fonction pour convertir les coordonnées d'écran en coordonnées complexes
+-- Function to convert screen coordinates to complex coordinates
 local function screenToComplex(x, y)
     local real = minReal + (maxReal - minReal) * (x / width)
     local imag = minImag + (maxImag - minImag) * (y / height)
     return real, imag
 end
 
--- Fonction pour vérifier si un point est à l'intérieur de l'ensemble de Mandelbrot
+-- Function to check if a point is inside the Mandelbrot set
 local function isInsideMandelbrot(real, imag, maxIterations)
     local zReal, zImag = 0, 0
     for i = 1, maxIterations do
         local zReal2, zImag2 = zReal * zReal, zImag * zImag
         zImag, zReal = 2 * zReal * zImag + imag, zReal2 - zImag2 + real
         if zReal2 + zImag2 > 4 then
+            -- The point is outside the set
             return false, i
         end
     end
+    -- The point is inside the set
     return true, maxIterations
 end
 
--- Parcours de tous les pixels de l'image
+-- Loop through all pixels of the image
 for y = 0, height - 1 do
     for x = 0, width - 1 do
         local real, imag = screenToComplex(x, y)
         local isInside, iterations = isInsideMandelbrot(real, imag, maxIterations)
-        -- Couleur du pixel en nuance de violet / bleu
+        -- Color of the pixel in shades of violet / blue
         local color = isInside and 0x000000 or 0x08000F * iterations
         profan.setpixel(x, y, color)
     end
