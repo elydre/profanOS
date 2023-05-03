325496861380ded3cb9af06dfdfa1cde65c225c4
pf4 <74263128+elydre@users.noreply.github.com>
2023-05-03 11:15:43 +0200

    add frexpf

--- zlibs/ports/math.c
+++ zlibs/ports/math.c
@@ -1275,9 +1275,24 @@ double frexp(double a, int *b) {
     return 0;
 }
 
-float frexpf(float a, int *b) {
-    printf("frexpf not implemented yet, WHY DO YOU USE IT ?\n");
-    return 0;
+float frexpf(float x, int *e) {
+    union { float f; uint32_t i; } y = { x };
+    int ee = y.i>>23 & 0xff;
+
+    if (!ee) {
+        if (x) {
+            x = frexpf(x*0x1p64, e);
+            *e -= 64;
+        } else *e = 0;
+        return x;
+    } else if (ee == 0xff) {
+        return x;
+    }
+
+    *e = ee - 0x7e;
+    y.i &= 0x807ffffful;
+    y.i |= 0x3f000000ul;
+    return y.f;
 }
 
 long double frexpl(long double a, int *b) {
