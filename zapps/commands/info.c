965e69f51d397a65c01d0a784585ed7f1b013375
pf4 <74263128+elydre@users.noreply.github.com>
2023-05-06 11:08:15 +0200

    Update info.c

--- zapps/commands/info.c
+++ zapps/commands/info.c
@@ -28,9 +28,9 @@ int main(int argc, char **argv) {
     printf("$4work time:  $1%d.%ds\n", (int) (c_timer_get_ms() / 1000), (int) (c_timer_get_ms() % 1000));
     printf("$4used mem:   $1%d.%dMo\n", used_mem / 1024, used_mem % 1024);
     printf("$4act alloc:  $1%d$7/$1%d\n", c_mem_get_info(4, 0) - c_mem_get_info(5, 0), c_mem_get_info(4, 0));
-    printf("$4phys mem:   $1%fMo\n", ((double) c_mem_get_info(0, 0) / 1024) / 1024);
-    printf("$4disk size:  $1%fMo\n", ((double) c_fs_get_sector_count()) / 2048);
-    printf("$4ramdisk:    $1%d%c $7($1%fMo$7)\n", (int)(100 * ((double) c_ramdisk_get_used()) / c_ramdisk_get_size()), '%', ((double) c_ramdisk_get_size()) / 2048);
+    printf("$4phys mem:   $1%gMo\n", ((double) c_mem_get_info(0, 0) / 1024) / 1024);
+    printf("$4disk size:  $1%gMo\n", ((double) c_fs_get_sector_count()) / 2048);
+    printf("$4ramdisk:    $1%d%c $7($1%gMo$7)\n", (int)(100 * ((double) c_ramdisk_get_used()) / c_ramdisk_get_size()), '%', ((double) c_ramdisk_get_size()) / 2048);
     printf("$4screen:     $1%dx$1%d\n", c_vesa_get_width(), c_vesa_get_height());
     return 0;
 }
