d0fa63191de97c76bde40b68c44abf9d206c8d23
pf4 <74263128+elydre@users.noreply.github.com>
2023-05-08 11:16:18 +0200

    0.12.3b - fb pitch

--- include/kernel/system.h
+++ include/kernel/system.h
@@ -3,7 +3,7 @@
 
 // build settings
 
-#define KERNEL_VERSION  "0.12.3"
+#define KERNEL_VERSION  "0.12.3b"
 #define KERNEL_EDITING  "generic"
 
 #define PROCESS_MAX     20          // max process count
