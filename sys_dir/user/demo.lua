ccf94c5caec144d90ec4eb23a8b1896b27762552
pf4 <74263128+elydre@users.noreply.github.com>
2023-06-11 18:21:26 +0200

    Update demo.lua

--- sys_dir/user/demo.lua
+++ sys_dir/user/demo.lua
@@ -5,23 +5,22 @@ win = require("win")
 wptr = win.window_create(100, 100, "Hello world!")
 win.desktop_refresh()
 
+-- sleep for 2 seconds
+calls.ms_sleep(2000)
+
+d = profan.ticks()
 
 -- draw a rectangle (with refresh)
-for i = 10, 20 do
-    for j = 10, 20 do
-        win.window_set_pixel(wptr, i, j, 0xFF0000)
+for i = 0, 99 do
+    for j = 0, 99 do
+        win.window_display_pixel(wptr, i, j, 0xFF0000)
     end
 end
 
-win.window_refresh(wptr)
+e = profan.ticks()
 
+print("Time: " .. (e - d) .. "ms")
 
--- draw a rectangle (without refresh)
-for i = 30, 40 do
-    for j = 10, 20 do
-        win.window_display_pixel(wptr, i, j, 0x00FF00)
-    end
-end
 
 -- sleep for 2 seconds
 calls.ms_sleep(2000)
