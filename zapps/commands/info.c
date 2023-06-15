6baad2cb3e00c36a4c0edc88b81ce1ee5245fcf5
pf4 <74263128+elydre@users.noreply.github.com>
2023-06-15 17:15:28 +0200

    Update info.c

--- zapps/commands/info.c
+++ zapps/commands/info.c
@@ -3,34 +3,79 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+#define pl_and_pf(f, ...) print_logo_line(); printf(f, __VA_ARGS__)
+
+#define LOGO_LINES 9
+#define INFO_LINES 9
+
+#define LOGO_START_LINE ((INFO_LINES - LOGO_LINES) / 2)
+
+char *LOGO[] = {
+    "       $3*$4*$3*$4*$3*     ",
+    "     $4*$3*$4*$3*$4*$3*$4*$3*$4*$3   ",
+    "    $4*$3*$4*$3*$4*$3*$4*$3*$4*$3*$4*$3  ",
+    "    $3*$4*$3*$4*$7/|\\$4*$3*$4*$3*  ",
+    "     $3*$4*$7//|\\\\$4*$3*   ",
+    "      $7///|\\\\\\$3    ",
+    "     $7////|\\\\\\\\$3   ",
+    "$A####$7///$A#####$7\\\\\\  ",
+    "   $2#$A####   $2#$A##$7\\\\ ",
+};
+
+int curent_line;
+int logo_line;
+
+void print_logo_line() {
+    // chek if we need to print a new line
+    if (LOGO_START_LINE + logo_line > curent_line || LOGO_START_LINE + LOGO_LINES <= curent_line) {
+        printf("                       "); 
+    } else {
+        printf("%s      ", LOGO[logo_line++]);
+    }
+
+    curent_line++;
+}
+
 int main(int argc, char **argv) {
+    printf("\n $2-$A Welcome to the profan Operating System $2-\n\n");
+
+    curent_line = 0;
+    logo_line = 0;
+
     char kver[32];
     c_sys_kinfo(kver);
-    printf("$4kernel:     $1%s\n", kver);
-    printf("$4FR time:    ");
+
     i_time_t time;
     c_time_get(&time);
-    char tmp[3];
-    for (int i = 2; i >= 0; i--) {
-        itoa(time.full[i], tmp, 10);
-        if (tmp[1] == '\0') { tmp[1] = tmp[0]; tmp[0] = '0'; }
-        printf("$1%s$7:", tmp);
-    }
-    c_kprint_backspace(); printf(" ");
-    for (int i = 3; i < 6; i++) {
-        itoa(time.full[i], tmp, 10);
-        if (tmp[1] == '\0') { tmp[1] = tmp[0]; tmp[0] = '0'; }
-        printf("$1%s$7/", tmp);
-    }
+
     int used_mem = c_mem_get_info(6, 0) / 1024;
-    c_kprint_backspace();
-    printf("$4\nunix time:  $1%d\n", time_gen_unix());
-    printf("$4work time:  $1%d.%ds\n", (int) (c_timer_get_ms() / 1000), (int) (c_timer_get_ms() % 1000));
-    printf("$4used mem:   $1%d.%dMo\n", used_mem / 1024, used_mem % 1024);
-    printf("$4act alloc:  $1%d$7/$1%d\n", c_mem_get_info(4, 0) - c_mem_get_info(5, 0), c_mem_get_info(4, 0));
-    printf("$4phys mem:   $1%gMo\n", ((double) c_mem_get_info(0, 0) / 1024) / 1024);
-    printf("$4disk size:  $1%gMo\n", ((double) c_fs_get_sector_count()) / 2048);
-    printf("$4ramdisk:    $1%d%c $7($1%gMo$7)\n", (int)(100 * ((double) c_ramdisk_get_used()) / c_ramdisk_get_size()), '%', ((double) c_ramdisk_get_size()) / 2048);
-    printf("$4screen:     $1%dx$1%d\n", c_vesa_get_width(), c_vesa_get_height());
+
+    pl_and_pf("$4kernel:     $2%s\n", kver);
+
+    pl_and_pf("$4UTC time:   $2%02d$7:$2%02d$7:$2%02d %02d$7/$2%02d$7/$2%02d\n",
+        time.hours, time.minutes, time.seconds,
+        time.day_of_month, time.month, time.year
+    );
+
+    pl_and_pf("$4work time:  $2%gs\n", c_timer_get_ms() / 1000.0);
+    pl_and_pf("$4used mem:   $2%d.%dMo\n", used_mem / 1024, used_mem % 1024);
+
+    pl_and_pf("$4act alloc:  $2%d$7/$2%d\n",
+        c_mem_get_info(4, 0) - c_mem_get_info(5, 0),
+        c_mem_get_info(4, 0)
+    );
+
+    pl_and_pf("$4phys mem:   $2%gMo\n", ((double) c_mem_get_info(0, 0) / 1024) / 1024);
+    pl_and_pf("$4disk size:  $2%gMo\n", ((double) c_fs_get_sector_count()) / 2048);
+
+    pl_and_pf("$4ramdisk:    $2%d%% $7($2%gMo$7)\n",
+        (int) (100 * ((double) c_ramdisk_get_used()) / c_ramdisk_get_size()),
+        c_ramdisk_get_size() / 2048.0
+    );
+
+    pl_and_pf("$4screen:     $2%dx$2%d\n", c_vesa_get_width(), c_vesa_get_height());
+
+    printf("\n");
+
     return 0;
 }
