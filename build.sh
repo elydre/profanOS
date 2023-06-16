10db01e46eb75a12f2930fbceca28723068cffa0
pf4 <74263128+elydre@users.noreply.github.com>
2023-06-16 18:32:34 +0200

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,12 +1,12 @@
 CCFLAGS="-m32 -ffreestanding -Wall -Wextra -fno-exceptions -fno-stack-protector -march=i686 -Wno-unused -I include/zlibs"
-LDFLAGS="-m elf_i386 -T tools/zlink.ld"
+LINKER_FLAGS="-b elf32i386 -nostdlib -T tools/zlink.ld"
 
 PROJET="demo"
 
-gcc $CCFLAGS -c test.c -o test.o
-ld $LDFLAGS -o $PROJET.elf out/make/zentry.o test.o
+tcc $CCFLAGS -c test.c -o test.o
+tcc $CCFLAGS -c tools/zentry.c -o zentry.o
+./vlink $LINKER_FLAGS -o $PROJET.elf zentry.o test.o
 
-objcopy -O binary $PROJET.elf $PROJET.bin -j .text -j .data -j .rodata -j .bss
-rm *.o *.elf
+objcopy -O binary $PROJET.elf $PROJET.bin -j .text -j .data -j .rodata
 
 cp demo.bin out/zapps/commands/
