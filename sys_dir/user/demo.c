f4cc792491f3f50482680333034f15d6f966bef5
pf4 <74263128+elydre@users.noreply.github.com>
2023-06-18 14:08:38 +0200

    Update demo.c

--- sys_dir/user/demo.c
+++ sys_dir/user/demo.c
@@ -1,3 +1,7 @@
-void main() {
-    ((int (*)(const char *, ...)) ((int (*)(int, int)) *(int *) 0x1ffffb)(1009, 38))("Hello World!\n");
+#include <stdio.h>
+
+int main() {
+    printf("Hello, World!\n");
+
+    return 0;
 }
