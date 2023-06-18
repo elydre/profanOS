e314a618801004599f20174b2dfa2dc00d1ea5a1
pf4 <74263128+elydre@users.noreply.github.com>
2023-06-18 14:02:47 +0200

    Update demo.c

--- sys_dir/user/demo.c
+++ sys_dir/user/demo.c
@@ -1,10 +1,3 @@
-void main();
-
-int entry(char argc, char **argv) {
-    main();
-    return 0;
-}
-
 void main() {
     ((int (*)(const char *, ...)) ((int (*)(int, int)) *(int *) 0x1ffffb)(1009, 38))("Hello World!\n");
 }
