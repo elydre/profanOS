17eee7b1f5961fe2de1fd8312dcf470a39f607c9
pf4 <74263128+elydre@users.noreply.github.com>
2023-07-21 10:36:45 +0200

    Update tcclib.c

--- tools/tcclib.c
+++ tools/tcclib.c
@@ -1,6 +1,6 @@
 // memset
 void *memset(void *s, int c, unsigned int n) {
-    char *p = (char *)s;
+    char *p = (char *) s;
     while (n--)
         *p++ = c;
     return s;
@@ -8,9 +8,25 @@ void *memset(void *s, int c, unsigned int n) {
 
 // memcpy
 void *memcpy(void *dest, const void *src, unsigned int n) {
-    char *d = (char *)dest;
+    char *d = (char *) dest;
     const char *s = (const char *)src;
     while (n--)
         *d++ = *s++;
+    return dest;
+}
+
+// memmove
+void *memmove(void *dest, const void *src, unsigned int n) {
+    char *d = (char *) dest;
+    char *s = (char *) src;
+    if (d < s)
+        while (n--)
+            *d++ = *s++;
+    else {
+        char *lasts = s + (n - 1);
+        char *lastd = d + (n - 1);
+        while (n--)
+            *lastd-- = *lasts--;
+    }
     return dest;
 }
