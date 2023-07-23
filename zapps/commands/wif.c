5d4c2ff81db02396bf5ee3958041662c6de45bd8
pf4 <74263128+elydre@users.noreply.github.com>
2023-07-23 16:06:16 +0200

    wif rework

--- zapps/commands/wif.c
+++ zapps/commands/wif.c
@@ -8,8 +8,8 @@
 
 
 int main(int argc, char** argv) {
-    if (argc < 3) {
-        puts("$BUsage: wif <file>\n");
+    if (argc != 4) {
+        puts("$BUsage: $3wif <file> <content>\n");
         return 0;
     }
 
@@ -17,16 +17,9 @@ int main(int argc, char** argv) {
     assemble_path(argv[1], argv[2], file);
 
     if (c_fs_does_path_exists(file) && c_fs_get_sector_type(c_fs_path_to_id(file)) == 2) {
-        char *char_content = malloc(256);
-
-        puts("-> ");
-        input(char_content, 256, c_blue);
-        puts("\n");
-
-        c_fs_write_in_file(file, (uint8_t *) char_content, strlen(char_content));
-        free(char_content);
+        c_fs_write_in_file(file, (uint8_t *) argv[3], strlen(argv[3]));
     } else {
-        printf("$3%s$B file not found\n", file);
+        printf("$Bfile $3%s$B not found\n", file);
     }
 
     free(file);
