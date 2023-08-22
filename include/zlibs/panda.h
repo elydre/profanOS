035ade86628f54b0cd4e9aaaee4df5b1045f7f22
pf4 <74263128+elydre@users.noreply.github.com>
2023-08-22 14:08:34 +0200

    update

--- include/zlibs/panda.h
+++ include/zlibs/panda.h
@@ -3,6 +3,6 @@
 
 #define get_func_addr ((int (*)(int, int)) *(int *) 0x1ffffb)
 
-#define panda_print_string ((void (*)(char *, int, char)) get_func_addr(PANDA_ID, 6))
+#define panda_print_string ((void (*)(char *, int, char)) get_func_addr(PANDA_ID, 8))
 
 #endif
--- zlibs/profan/panda.c
+++ zlibs/profan/panda.c
@@ -13,7 +13,26 @@ typedef struct {
     uint8_t *data;
 } font_data_t;
 
-font_data_t *g_font;
+typedef struct {
+    char content;
+    char color;
+} screen_char_t;
+
+typedef struct {
+    uint32_t cursor_x;
+    uint32_t cursor_y;
+    uint32_t scroll_offset;
+
+    uint32_t max_lines;
+    uint32_t max_cols;
+
+    screen_char_t *screen_buffer;
+    
+    font_data_t *font;
+} panda_global_t;
+
+
+panda_global_t *g_panda;
 
 void init_panda();
 
@@ -74,23 +93,6 @@ void free_font(font_data_t *pff) {
     free(pff);
 }
 
-void print_char(uint32_t xo, uint32_t yo, char c, uint32_t color) {
-    uint8_t *char_data = g_font->data + (c * g_font->charsize);
-
-    uint32_t x = 0;
-    uint32_t y = 0;
-    for (uint32_t i = 0; i < g_font->charsize; i++) {
-        if (x >= g_font->width) {
-            x = 0;
-            y++;
-        }
-        for (int j = 7; j >= 0; j--) {
-            c_vesa_set_pixel(xo + x, yo + y, char_data[i] & (1 << j) ? color : 0);
-            x++;
-        }
-    }
-}
-
 uint32_t compute_color(char color) {
     switch (color) {
         case 0x0: return 0x000000;
@@ -112,29 +114,131 @@ uint32_t compute_color(char color) {
     }
 }
 
-void panda_print_string(char *string, int len, char color) {
-    static uint32_t x = 0;
-    static uint32_t y = 0;
+void print_char(uint32_t xo, uint32_t yo, char c, char color_code) {
+    uint32_t color = compute_color(color_code);
 
-    for (int i = 0; (len < 0) ? (string[i]) : (i < len); i++) {
-        if (string[i] == '\n') {
+    uint8_t *char_data = g_panda->font->data + (c * g_panda->font->charsize);
+
+    uint32_t x = 0;
+    uint32_t y = 0;
+    for (uint32_t i = 0; i < g_panda->font->charsize; i++) {
+        if (x >= g_panda->font->width) {
             x = 0;
-            y += g_font->height;
-            if (y >= c_vesa_get_height() - g_font->height) {
-                y = 0;
-            }
-        } else {
-            print_char(x, y, string[i], compute_color(color));
-            x += g_font->width;
+            y++;
+        }
+        for (int j = 7; j >= 0; j--) {
+            c_vesa_set_pixel(xo + x, yo + y, char_data[i] & (1 << j) ? color : 0);
+            if (x >= g_panda->font->width) break;
+            x++;
         }
     }
 }
 
 
+void compute_ansi_escape(char *str) {
+    c_kprint("compute_ansi_escape\n");
+    char *ptr = str;
+    while (*ptr != '\0') {
+        if (*ptr >= '0' && *ptr <= '9') {
+            ptr++;
+        } else if (*ptr == ';') {
+            g_panda->cursor_x = atoi(str);
+            str = ptr + 1;
+            ptr = str;
+        } else if (*ptr == 'H') {
+            g_panda->cursor_y = atoi(str);
+            return;
+        } else {
+            return;
+        }
+    }
+}
+
+void panda_scroll() {
+    // fill the rest of the line with spaces
+    int offset;
+    while (g_panda->cursor_x < g_panda->max_cols) {
+        offset = (g_panda->cursor_y - g_panda->scroll_offset) * g_panda->max_cols + g_panda->cursor_x;
+        if (g_panda->screen_buffer[offset].content == '\0') {
+            g_panda->screen_buffer[offset].content = ' ';
+        }
+        g_panda->cursor_x++;
+    }
+
+    g_panda->cursor_x = 0;
+    g_panda->cursor_y++;
+
+    if (g_panda->cursor_y - g_panda->scroll_offset < g_panda->max_lines) {
+        return;
+    }
+    g_panda->scroll_offset++;
+
+    // scroll the display and print it
+    int new_offset;
+    for (uint32_t i = 0; i < g_panda->max_lines - 1; i++) {
+        for (uint32_t j = 0; j < g_panda->max_cols; j++) {
+            new_offset = i * g_panda->max_cols + j;
+            offset = new_offset + g_panda->max_cols;
+            if (g_panda->screen_buffer[new_offset].content == g_panda->screen_buffer[offset].content)
+                continue;
+            g_panda->screen_buffer[new_offset].content = g_panda->screen_buffer[offset].content;
+            g_panda->screen_buffer[new_offset].color = g_panda->screen_buffer[offset].color;
+            print_char(j * g_panda->font->width, i * g_panda->font->height, g_panda->screen_buffer[new_offset].content, g_panda->screen_buffer[new_offset].color);
+        }
+    }
+
+    // clear the last line
+    for (uint32_t j = 0; j < g_panda->max_cols; j++) {
+        new_offset = (g_panda->max_lines - 1) * g_panda->max_cols + j;
+        if (g_panda->screen_buffer[new_offset].content == ' ') continue;
+        g_panda->screen_buffer[new_offset].content = ' ';
+        g_panda->screen_buffer[new_offset].color = 0xF;
+        print_char(j * g_panda->font->width, (g_panda->max_lines - 1) * g_panda->font->height, g_panda->screen_buffer[new_offset].content, g_panda->screen_buffer[new_offset].color);
+    }
+}
+
+void panda_print_string(char *string, int len, char color) {
+    uint32_t y;
+    for (int i = 0; (len < 0) ? (string[i]) : (i < len); i++) {
+        if (string[i] == '\n') {
+            panda_scroll();
+        } else if (string[i] == '\033') {
+            i++;
+            if (string[i] == '[') {
+                i++;
+                compute_ansi_escape(string + i);
+            }
+        }
+        else {
+            y = g_panda->cursor_y - g_panda->scroll_offset;
+            g_panda->screen_buffer[y * g_panda->max_cols + g_panda->cursor_x].content = string[i];
+            g_panda->screen_buffer[y * g_panda->max_cols + g_panda->cursor_x].color = color;
+            print_char(g_panda->cursor_x * g_panda->font->width,
+                y * g_panda->font->height,
+                string[i],
+                color
+            );
+            g_panda->cursor_x++;
+        }
+    }
+}
+
 void init_panda() {
-    g_font = load_psf_font("/zada/fonts/lat15-bold24.psf");
-    if (g_font == NULL) {
+    g_panda = malloc(sizeof(panda_global_t));
+    g_panda->font = load_psf_font("/zada/fonts/lat38-bold18.psf");
+
+    if (g_panda->font == NULL) {
         printf("Failed to load font\n");
     }
+
+    g_panda->cursor_x = 0;
+    g_panda->cursor_y = 0;
+    g_panda->scroll_offset = 0;
+
+    g_panda->max_lines = c_vesa_get_height() / g_panda->font->height;
+    g_panda->max_cols = c_vesa_get_width() / g_panda->font->width;
+
+    g_panda->screen_buffer = calloc(g_panda->max_lines * g_panda->max_cols, sizeof(screen_char_t));
+
     printf("Init of panda terminal emulator !\n");
 }
