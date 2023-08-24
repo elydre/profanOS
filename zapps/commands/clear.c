ef796f2dfc10531b76109a2e254cd52c9cd0119f
pf4 <74263128+elydre@users.noreply.github.com>
2023-08-24 11:30:25 +0200

    Update clear.c

--- zapps/commands/clear.c
+++ zapps/commands/clear.c
@@ -1,6 +1,43 @@
+#include <syscall.h>
 #include <stdio.h>
 
-int main(void) {
+int ansiclear() {
     printf("\033[2J");
+    return 0;
+}
+
+int hardclear() {
+    c_clear_screen();
+    ansiclear();
+    return 0;
+}
+
+int showhelp() {
+    puts(
+        "Usage: clear [OPTION]\n"
+        " -a   use ANSI escape codes to clear the screen\n"
+        " -h   show this help message\n"
+        " -x   set all pixels to black and clear the screen\n"
+    );
+    return 0;
+}
+
+int main(int argc, char **argv) {
+    argc--;
+    argv++;
+
+    if (argc == 1) return ansiclear();
+    if (argc > 2) return showhelp();
+    if (argv[1][0] != '-') return showhelp();
+    switch (argv[1][1]) {
+        case 'a':
+            return ansiclear();
+        case 'h':
+            return showhelp();
+        case 'x':
+            return hardclear();
+        default:
+            return showhelp();
+    }
     return 0;
 }
