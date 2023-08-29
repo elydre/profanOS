f33540e43a3391ddfdd4433a2d9cafee0f31af0b
pf4 <74263128+elydre@users.noreply.github.com>
2023-08-29 17:52:27 +0200

    Update profan.c

--- zlibs/profan/profan.c
+++ zlibs/profan/profan.c
@@ -13,39 +13,6 @@ int main() {
 }
 
 void assemble_path(char *old, char *new, char *result) {
-    /*
-    result[0] = '\0';
-    if (new[0] != '/') {
-        strcpy(result, old);
-    }
-    if (result[strlen(result) - 1] != '/') {
-        strncat(result, "/", 1);
-    }
-    int index;
-    for (uint32_t i = 0; i < strlen(new); i++) {
-        if (new[i] == '.' && new[i + 1] == '.' && (new[i + 2] == '/' || new[i + 2] == '\0')) {
-            for (int j = strlen(result) - 2; j >= 0; j--) {
-                if (result[j] == '/') {
-                    result[j + 1] = '\0';
-                    break;
-                }
-            }
-            i += 2;
-        } else if (new[i] == '.' && new[i + 1] == '/') {
-            i++;
-        } else if (new[i] == '/' && result[strlen(result) - 1] == '/') {
-            continue;
-        } else {
-            index = strlen(result);
-            result[index] = new[i];
-            result[index + 1] = '\0';
-        }
-    }
-    if (result[strlen(result) - 1] == '/' && strlen(result) != 1) {
-        result[strlen(result) - 1] = '\0';
-    }
-    */
-
     result[0] = '\0';
     if (new[0] == '/') {
         strcpy(result, new);
