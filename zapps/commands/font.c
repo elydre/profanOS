a0f2a9377a746460066c5c46e17a886216fd6372
pf4 <74263128+elydre@users.noreply.github.com>
2023-08-29 17:52:44 +0200

    Create font.c

---
+++ zapps/commands/font.c
@@ -0,0 +1,36 @@
+#include <filesys.h>
+#include <profan.h>
+#include <panda.h>
+
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+    char *pwd = argv[1];
+    argc--;
+    argv++;
+
+    if (argc != 2) {
+        printf("Usage: font [font path]\n");
+        return 1;
+    }
+
+    char *full_path = malloc(256);
+    assemble_path(pwd, argv[1], full_path);
+
+    sid_t file = fu_path_to_sid(ROOT_SID, full_path);
+    if (IS_NULL_SID(file) || !fu_is_file(file)) {
+        printf("File not found: %s\n", full_path);
+        free(full_path);
+        return 1;
+    }
+
+    if (panda_change_font(full_path)) {
+        printf("Failed to change font, invalid file\n");
+        free(full_path);
+        return 1;
+    }
+
+    free(full_path);
+    return 0;
+}
