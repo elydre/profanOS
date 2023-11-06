030ff4f64528ee7ad0b2c4958bbe1d7cdda5aea1
pf4 <74263128+elydre@users.noreply.github.com>
2023-11-06 15:23:15 +0100

    time: add localtime/mktime/time

--- zlibs/ports/time.c
+++ zlibs/ports/time.c
@@ -1,14 +1,34 @@
 #include <syscall.h>
+#include <stdlib.h>
 #include <stdio.h>
 #include <type.h>
 
 #define TIME_C
 #include <time.h>
 
-int       daylight;
-long int  timezone;
-char     **tzname;
+#define is_leap_year(year) ((year % 4 == 0 && year % 100 != 0) || year % 400 == 0)
+static uint32_t seconde_in_month[] = {
+    0,
+    2678400,
+    5097600,
+    7776000,
+    10368000,
+    13046400,
+    15638400,
+    18316800,
+    20995200,
+    23587200,
+    26265600,
+    28857600
+};
 
+#define seconde_in_year 31536000
+#define seconde_in_leap_year 31622400
+#define seconde_in_day 86400
+#define seconde_in_hour 3600
+#define seconde_in_minute 60
+#define start_year 1970
+#define century 2000
 
 int main(void) {
     return 0;
@@ -75,8 +95,9 @@ tm_t *gmtime_r(const time_t *a, tm_t *b) {
 }
 
 tm_t *localtime(const time_t *a) {
-    puts("Localtime is not implemented yet, WHY DO YOU USE IT ?");
-    return NULL;
+    tm_t *time = malloc(sizeof(tm_t));
+    c_time_get(time);
+    return time;    
 }
 
 tm_t *localtime_r(const time_t *a, tm_t *b) {
@@ -84,9 +105,30 @@ tm_t *localtime_r(const time_t *a, tm_t *b) {
     return NULL;
 }
 
-time_t mktime(tm_t *a) {
-    puts("Mktime is not implemented yet, WHY DO YOU USE IT ?");
-    return 0;
+time_t mktime(tm_t *time) {
+    time_t unix_time = 0;
+
+    // Add seconds
+    unix_time += time->tm_sec;
+
+    // Add minutes
+    unix_time += time->tm_min * seconde_in_minute;
+
+    // Add hours
+    unix_time += time->tm_hour * seconde_in_hour;
+
+    // Add days
+    unix_time += (time->tm_mday - 1) * seconde_in_day;
+
+    // Add months
+    unix_time += seconde_in_month[time->tm_mon];
+
+    // Add years
+    for (int i = start_year; i < time->tm_year + century; i++) {
+        unix_time += is_leap_year(i) ? seconde_in_leap_year : seconde_in_year;
+    }
+
+    return unix_time;
 }
 
 int nanosleep(const timespec_t *a, timespec_t *b) {
@@ -105,8 +147,13 @@ char *strptime(const char *a, const char *b, tm_t *c) {
 }
 
 time_t time(time_t *a) {
-    c_serial_print(SERIAL_PORT_A, "Time is not implemented yet, WHY DO YOU USE IT ?\n");
-    return 0;
+    tm_t time;
+    c_time_get(&time);
+
+    if (a != NULL) {
+        *a = mktime(&time);
+    }
+    return mktime(&time);
 }
 
 int timer_create(clockid_t a, sigevent_t *b, timer_t c) {
