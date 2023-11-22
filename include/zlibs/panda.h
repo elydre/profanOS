6921ca794cdc16744c89bb0e58f8486b31dca2bc
pf4 <74263128+elydre@users.noreply.github.com>
2023-11-22 16:57:16 +0100

    Update panda.h

--- include/zlibs/panda.h
+++ include/zlibs/panda.h
@@ -8,7 +8,7 @@
 #define panda_print_string ((void (*)(char *, int)) get_func_addr(PANDA_ID, 11))
 #define panda_set_start ((void (*)(int)) get_func_addr(PANDA_ID, 12))
 #define panda_get_cursor ((void (*)(uint32_t *, uint32_t *)) get_func_addr(PANDA_ID, 13))
-#define panda_get_size ((void (*)(uint32_t *, uint32_t *)) get_func_addr(PANDA_ID, 15))
-#define panda_change_font ((int (*)(char *)) get_func_addr(PANDA_ID, 16))
+#define panda_get_size ((void (*)(uint32_t *, uint32_t *)) get_func_addr(PANDA_ID, 14))
+#define panda_change_font ((int (*)(char *)) get_func_addr(PANDA_ID, 15))
 
 #endif
