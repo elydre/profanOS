ae1493b64b749856f9fe08cf680e55704ea76f00
pf4 <74263128+elydre@users.noreply.github.com>
2023-12-06 18:35:08 +0100

    Update speed.c

--- zapps/tools/speed.c
+++ zapps/tools/speed.c
@@ -1,10 +1,35 @@
 #include <syscall.h>
+#include <string.h>
 #include <stdio.h>
 
 #define LOOPS 100000000
 
+int check_args(int argc, char **argv) {
+    if (argc == 2 && strcmp(argv[1], "0") == 0) {
+        return 0;
+    } else if (argc == 2 && strcmp(argv[1], "1") == 0) {
+        return 1;
+    }
+    
+    puts("Usage: speed [0|1]\n"
+        "  0: keep sheduler enabled during the loop\n"
+        "  1: disable sheduler during the loop\n\n"
+        "This program will do a loop and print the time it took\n"
+        "to do the loop. The loop is done 100 million times.\n"
+        "The cpu speed is calculated by dividing the number of\n"
+        "loops by the time it took to do the loop."
+    );
+
+    return 2;
+}
+
 int main(int argc, char **argv) {
-    c_process_set_sheduler(0);
+    int mode = check_args(argc, argv);
+    if (mode == 2)
+        return 1;
+
+    if (mode)
+        c_process_set_sheduler(0);
 
     // get the time before the loop
     int start = c_timer_get_ms();
@@ -17,7 +42,8 @@ int main(int argc, char **argv) {
     // get the time after the loop
     int end = c_timer_get_ms();
 
-    c_process_set_sheduler(1);
+    if (mode)
+        c_process_set_sheduler(1);
 
     // print the time difference
     printf("loop time: %dms\n", end - start);
