30927c44ff7049a5e83b536edc5a2ab3d8ab065f
pf4 <74263128+elydre@users.noreply.github.com>
2024-01-11 18:22:49 +0100

    top: add process list

--- zapps/commands/top.c
+++ zapps/commands/top.c
@@ -1,8 +1,11 @@
 #include <syscall.h>
 #include <unistd.h>
+#include <stdlib.h>
 #include <string.h>
 #include <stdio.h>
 #include <time.h>
+
+#define PROCESS_MAX 20
 
 void str_insert(char *str, char *insert, int pos) {
     int len = strlen(str);
@@ -38,11 +41,84 @@ void draw_line(char *buf, int percent) {
     str_insert(buf, "\033[31m", 1);
 }
 
+char *get_state(int state) {
+    switch (state) {
+        case 0: return "RUNNING";
+        case 1: return "WAITING";
+        case 2: return "TSLEEP";
+        case 3: return "FSLEEP";
+        case 4: return "ZOMBIE";
+        case 5: return "DEAD";
+        case 6: return "IDLE";
+        default: return "UNKNOWN";
+    }
+}
 
-int main(void) {
-    int total_mem, used_mem, cpu;
+void sort_tab(uint32_t *tab, int size) {
+    uint32_t tmp;
+    for (int i = 0; i < size; i++) {
+        for (int j = i; j > 0 && tab[j] < tab[j - 1]; j--) {
+            tmp = tab[j];
+            tab[j] = tab[j - 1];
+            tab[j - 1] = tmp;
+        }
+    }
+}
+
+void list_process(void) {
+    uint32_t pid_list[PROCESS_MAX]; // it's a define
+    char info_line[81];
+    int pid_list_len = c_process_generate_pid_list(pid_list, PROCESS_MAX);
+    sort_tab(pid_list, pid_list_len);
+
+    int line_len = snprintf(info_line, 81, "  profanOS . kernel %s . %d processes . uptime %ds (%ds IDLE)",
+            c_sys_kinfo(),
+            pid_list_len - 2,
+            c_timer_get_ms() / 1000,
+            c_process_get_run_time(1) / 1000
+    );
+
+    // center info line
+    int spaces = (79 - line_len) / 2;
+    memmove(info_line + spaces, info_line, line_len);
+    memset(info_line, ' ', spaces);
+    info_line[80] = '\0';
+    puts(info_line);
+
+    puts("\e[47m\e[30m PID PPID     STATE     TIME      MEM ALLOC NAME                                \e[0m");
+    int pid;
+    char name[64];
+    for (int i = 0; i < pid_list_len; i++) {
+        pid = pid_list[i];
+        if (pid == 1) continue;
+        c_process_get_name(pid, name);
+        printf("%4d %4d %9s %7gs %7dK %5d %-36s\n",
+                pid,
+                c_process_get_ppid(pid),
+                get_state(c_process_get_state(pid)),
+                c_process_get_run_time(pid) / 1000.0,
+                c_mem_get_info(8, pid) / 1024,
+                c_mem_get_info(7, pid),
+                name
+        );
+    }
+    printf("%80s\n", "");
+}
+
+int check_arg(int argc, char **argv) {
+    if (argc > 2 || (argc == 2 && (argv[1][0] < '0' || argv[1][0] > '9'))) {
+        printf("Usage: %s [sleep]\n", argv[0]);
+        exit(1);
+    }
+    return argc == 2 ? atoi(argv[1]) : 1;
+}
+
+int main(int argc, char **argv) {
+    int total_mem, used_mem, cpu, to_sleep;
     int last_idle, idle, last_total, total;
     char buf[64];
+
+    to_sleep = check_arg(argc, argv);
 
     idle = total = 0;
 
@@ -76,10 +152,9 @@ int main(void) {
 
         sprintf(buf, "%dM/%dM", used_mem / 1024 / 1024, total_mem / 1024 / 1024);
         draw_line(buf, used_mem * 100 / total_mem);
-        printf("%s %d allocs\n", buf, c_mem_get_info(4, 0) - c_mem_get_info(5, 0));
-
-        fflush(stdout);
-        sleep(1);
+        printf("%s %d allocs    \n\n", buf, c_mem_get_info(4, 0) - c_mem_get_info(5, 0));
+        list_process();
+        sleep(to_sleep);
     }
     return 0;
 }
