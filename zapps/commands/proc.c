1feff997ea322fcd36bcadbd4e39fee3879a8055
pf4 <74263128+elydre@users.noreply.github.com>
2024-01-14 18:44:34 +0100

    proc: update print style

--- zapps/commands/proc.c
+++ zapps/commands/proc.c
@@ -7,14 +7,14 @@
 
 char *get_state(int state) {
     switch (state) {
-        case 0: return "RUNNING";
-        case 1: return "WAITING";
-        case 2: return "TSLEEP";
-        case 3: return "FSLEEP";
-        case 4: return "ZOMBIE";
-        case 5: return "DEAD";
-        case 6: return "IDLE";
-        default: return "UNKNOWN";
+        case 0: return "running";
+        case 1: return "waiting";
+        case 2: return "Tsleep";
+        case 3: return "Fsleep";
+        case 4: return "zombie";
+        case 5: return "dead";
+        case 6: return "idle";
+        default: return "unknown";
     }
 }
 
@@ -34,19 +34,20 @@ void list_process(void) {
     int pid_list_len = c_process_generate_pid_list(pid_list, PROCESS_MAX);
     sort_tab(pid_list, pid_list_len);
 
+    puts(" PID PPID     STATE     TIME      MEM ALLOC NAME");
     int pid;
     char name[64];
     for (int i = 0; i < pid_list_len; i++) {
         pid = pid_list[i];
         c_process_get_name(pid, name);
-        printf("pid: %d, ppid: %d, state: %s, name: %s, run: %gs, memory: %d Ko (%d alloc)\n",
+        printf("%4d %4d %9s %7gs %7dK %5d %-36s\n",
                 pid,
                 c_process_get_ppid(pid),
                 get_state(c_process_get_state(pid)),
-                name,
                 c_process_get_run_time(pid) / 1000.0,
                 c_mem_get_info(8, pid) / 1024,
-                c_mem_get_info(7, pid)
+                c_mem_get_info(7, pid),
+                name
         );
     }
 }
