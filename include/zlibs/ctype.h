4781c3fe53b11514d23930aec0388115b4cc8a1c
pf4 <74263128+elydre@users.noreply.github.com>
2024-02-10 20:21:41 +0100

    Update ctype.h

--- include/zlibs/ctype.h
+++ include/zlibs/ctype.h
@@ -2,5 +2,7 @@
 #define CTYPE_H
 
 #define isdigit(c) ((c) >= '0' && (c) <= '9')
+#define isalpha(c) (((c) >= 'a' && (c) <= 'z') || ((c) >= 'A' && (c) <= 'Z'))
+#define isalnum(c) (isdigit(c) || isalpha(c))
 
 #endif
