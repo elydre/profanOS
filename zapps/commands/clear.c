748a591ddc5a8129f688a5b178fc08e3b66ecbd9
pf4 <74263128+elydre@users.noreply.github.com>
2024-02-24 13:52:18 +0100

    Update clear.c

--- zapps/commands/clear.c
+++ zapps/commands/clear.c
@@ -1,13 +1,15 @@
 #include <syscall.h>
 #include <stdio.h>
 
+#define ANSI_CLEAR "\e[2J\e[H"
+
 int ansiclear(void) {
-    printf("\e[2J");
+    printf(ANSI_CLEAR);
     return 0;
 }
 
 int hardclear(void) {
-    c_kprint("\e[2J");
+    c_kprint(ANSI_CLEAR);
     ansiclear();
     return 0;
 }
