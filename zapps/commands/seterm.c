f6259810fb0f189c0177ca33bbc06706d79cc8d2
pf4 <74263128+elydre@users.noreply.github.com>
2024-02-24 13:52:21 +0100

    Create seterm.c

---
+++ zapps/commands/seterm.c
@@ -0,0 +1,59 @@
+#include <filesys.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <profan.h>
+#include <stdio.h>
+
+int is_valid_file(char *path) {
+    sid_t sid = fu_path_to_sid(ROOT_SID, path);
+    if (IS_NULL_SID(sid)) {
+        printf("seterm: %s: No such file or directory\n", path);
+        return 0;
+    }
+    if (!(fu_is_file(sid) || fu_is_fctf(sid))) {
+        printf("seterm: %s: Not a file or a fctf\n", path);
+        return 0;
+    }
+    return 1;
+}
+
+char *process_path(char *path) {
+    char *cwd = getenv("PWD");
+    if (cwd == NULL)
+        return NULL;
+    return assemble_path(cwd, path);
+}
+
+int main(int argc, char **argv) {
+    if (argc != 2 || argv[1][0] == '-') {
+        printf("Usage: seterm <file>\n", argv[0]);
+        return 1;
+    }
+
+    char *path = process_path(argv[1]);
+
+    if (path == NULL) {
+        printf("seterm: PWD not set\n");
+        return 1;
+    }
+
+    if (!is_valid_file(argv[1])) {
+        free(path);
+        return 1;
+    }
+
+    fm_reopen(3, path);
+    fm_reopen(4, path);
+    fm_reopen(5, path);
+
+    int ppid = getppid();
+
+    fm_reopen(fm_resol012(0, ppid), path);
+    fm_reopen(fm_resol012(1, ppid), path);
+    fm_reopen(fm_resol012(2, ppid), path);
+
+    setenv("TERM", path, 1);
+
+    free(path);
+    return 0;
+}
