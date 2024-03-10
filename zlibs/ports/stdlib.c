f420df773b68991bf75156f1c58127c221d394de
pf4 <74263128+elydre@users.noreply.github.com>
2024-03-10 15:13:30 +0100

    stdlib: strtol_l + strtol

--- zlibs/ports/stdlib.c
+++ zlibs/ports/stdlib.c
@@ -7,6 +7,14 @@
 #define STDLIB_C
 #include <stdlib.h>
 
+#ifndef LONG_MAX
+#define LONG_MAX 0x7fffffffL
+#endif
+
+#ifndef LONG_MIN
+#define LONG_MIN (-LONG_MAX - 1L)
+#endif
+
 uint32_t rand_seed = 0;
 char **g_env;
 
@@ -695,16 +703,103 @@ long double strtof_l(const char* str, char** end, locale_t loc) {
     return 0;
 }
 
-long int strtol(const char* str, char** end, int base) {
-    puts("strtol not implemented yet, WHY DO YOU USE IT ?");
-    return 0;
+long int strtol_l(const char *str, char **end, int base, locale_t loc);
+long int strtol(const char *str, char **end, int base) {
+    return strtol_l(str, end, base, NULL);
 }
 
-long long int strtol_l(const char* str, char** end, int base, locale_t loc) {
-    puts("strtol_l not implemented yet, WHY DO YOU USE IT ?");
-    return 0;
-}
+long int strtol_l(const char *str, char **end, int base, locale_t loc) {
+    const char *s;
+    unsigned long acc;
+    char c;
+    unsigned long cutoff;
+    int neg, any, cutlim;
 
+    // NORMALIZE_LOCALE(loc);
+    /*
+     * Skip white space and pick up leading +/- sign if any.
+     * If base is 0, allow 0x for hex and 0 for octal, else
+     * assume decimal; if base is already 16, allow 0x.
+     */
+    s = str;
+    do {
+        c = *s++;
+    } while (isspace((unsigned char) c));
+    if (c == '-') {
+        neg = 1;
+        c = *s++;
+    } else {
+        neg = 0;
+        if (c == '+')
+            c = *s++;
+    }
+    if ((base == 0 || base == 16) &&
+        c == '0' && (*s == 'x' || *s == 'X') &&
+        ((s[1] >= '0' && s[1] <= '9') ||
+        (s[1] >= 'A' && s[1] <= 'F') ||
+        (s[1] >= 'a' && s[1] <= 'f'))) {
+        c = s[1];
+        s += 2;
+        base = 16;
+    }
+    if (base == 0)
+        base = c == '0' ? 8 : 10;
+    acc = any = 0;
+    if (base < 2 || base > 36)
+        goto noconv;
+
+    /*
+     * Compute the cutoff value between legal numbers and illegal
+     * numbers.  That is the largest legal value, divided by the
+     * base.  An input number that is greater than this value, if
+     * followed by a legal input character, is too big.  One that
+     * is equal to this value may be valid or not; the limit
+     * between valid and invalid numbers is then based on the last
+     * digit.  For instance, if the range for longs is
+     * [-2147483648..2147483647] and the input base is 10,
+     * cutoff will be set to 214748364 and cutlim to either
+     * 7 (neg==0) or 8 (neg==1), meaning that if we have accumulated
+     * a value > 214748364, or equal but the next digit is > 7 (or 8),
+     * the number is too big, and we will return a range error.
+     *
+     * Set 'any' if any `digits' consumed; make it negative to indicate
+     * overflow.
+     */
+    cutoff = neg ? (unsigned long) - (LONG_MIN + LONG_MAX) + LONG_MAX
+        : LONG_MAX;
+    cutlim = cutoff % base;
+    cutoff /= base;
+    for ( ; ; c = *s++) {
+        if (c >= '0' && c <= '9')
+            c -= '0';
+        else if (c >= 'A' && c <= 'Z')
+            c -= 'A' - 10;
+        else if (c >= 'a' && c <= 'z')
+            c -= 'a' - 10;
+        else
+            break;
+        if (c >= base)
+            break;
+        if (any < 0 || acc > cutoff || (acc == cutoff && c > cutlim))
+            any = -1;
+        else {
+            any = 1;
+            acc *= base;
+            acc += c;
+        }
+    }
+    if (any < 0) {
+        acc = neg ? LONG_MIN : LONG_MAX;
+        // errno = ERANGE;
+    } else if (!any) {
+noconv:
+        // errno = EINVAL;
+    } else if (neg)
+        acc = -acc;
+    if (end != NULL)
+        *end = (char *)(any ? s - 1 : str);
+    return acc;
+}
 
 long long strtoll(const char* str, char** end, int base) {
     puts("strtoll not implemented yet, WHY DO YOU USE IT ?");
