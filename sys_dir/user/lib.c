089cc1f142095b6131fd57346435b8c40955836c
pf4 <74263128+elydre@users.noreply.github.com>
2024-03-15 14:00:56 +0100

    oui

--- sys_dir/user/lib.c
+++ sys_dir/user/lib.c
@@ -1,5 +1,3 @@
-int oui = 42;
-
 int add(int a, int b) {
-    return a + b + oui;
+    return a + b;
 }
--- zapps/commands/test.c
+++ zapps/commands/test.c
@@ -1,3 +1,4 @@
+#include <syscall.h>
 #include <stdlib.h>
 #include <profan.h>
 #include <string.h>
@@ -7,39 +8,39 @@
 
 // Elf32_Ehdr
 typedef struct {
-    unsigned char e_ident[16];    // ELF identification
-    unsigned short e_type;        // Object file type
-    unsigned short e_machine;    // Machine type
-    unsigned int e_version;        // Object file version
-    unsigned int e_entry;        // Entry point address
-    unsigned int e_phoff;        // Program header offset
-    unsigned int e_shoff;        // Section header offset
-    unsigned int e_flags;        // Processor-specific flags
-    unsigned short e_ehsize;        // ELF header size
-    unsigned short e_phentsize;    // Size of program header entry
-    unsigned short e_phnum;        // Number of program header entries
-    unsigned short e_shentsize;    // Size of section header entry
-    unsigned short e_shnum;        // Number of section header entries
-    unsigned short e_shstrndx;    // Section name string table index
+    unsigned char e_ident[16];  // ELF identification
+    unsigned short e_type;      // Object file type
+    unsigned short e_machine;   // Machine type
+    unsigned int e_version;     // Object file version
+    unsigned int e_entry;       // Entry point address
+    unsigned int e_phoff;       // Program header offset
+    unsigned int e_shoff;       // Section header offset
+    unsigned int e_flags;       // Processor-specific flags
+    unsigned short e_ehsize;    // ELF header size
+    unsigned short e_phentsize; // Size of program header entry
+    unsigned short e_phnum;     // Number of program header entries
+    unsigned short e_shentsize; // Size of section header entry
+    unsigned short e_shnum;     // Number of section header entries
+    unsigned short e_shstrndx;  // Section name string table index
 } Elf32_Ehdr;
 
 // Elf32_Shdr
 typedef struct {
-    unsigned int sh_name;        // Section name (string tbl index)
-    unsigned int sh_type;        // Section type
-    unsigned int sh_flags;        // Section flags
-    unsigned int sh_addr;        // Address where section is to be loaded
-    unsigned int sh_offset;        // File offset of section data
-    unsigned int sh_size;        // Size of section data
-    unsigned int sh_link;        // Section index linked to this section
-    unsigned int sh_info;        // Extra information
-    unsigned int sh_addralign;    // Section alignment
+    unsigned int sh_name;       // Section name (string tbl index)
+    unsigned int sh_type;       // Section type
+    unsigned int sh_flags;      // Section flags
+    unsigned int sh_addr;       // Address where section is to be loaded
+    unsigned int sh_offset;     // File offset of section data
+    unsigned int sh_size;       // Size of section data
+    unsigned int sh_link;       // Section index linked to this section
+    unsigned int sh_info;       // Extra information
+    unsigned int sh_addralign;  // Section alignment
     unsigned int sh_entsize;    // Entry size if section holds table
 } Elf32_Shdr;
 
 // dymsym
 typedef struct {
-    unsigned int st_name;        // Symbol name (string tbl index)
+    unsigned int st_name;       // Symbol name (string tbl index)
     unsigned int st_value;      // Symbol value
     unsigned int st_size;       // Symbol size
     unsigned char st_info;      // Symbol type and binding
@@ -56,10 +57,12 @@ typedef struct {
 #define SELFMAG         4
 #define ET_DYN          3
 #define EM_386          3
+#define SHT_PROGBITS    1
 
 typedef struct {
     uint32_t size;
-    uint8_t *data;
+    uint8_t *file;
+    uint8_t *mem;
 } dl_t;
 
 int is_valid_elf(void *data) {
@@ -76,6 +79,32 @@ int is_valid_elf(void *data) {
     return 1;
 }
 
+int load_sections(dl_t *dl) {
+    Elf32_Ehdr *ehdr = (Elf32_Ehdr *)dl->file;
+    Elf32_Shdr *shdr = (Elf32_Shdr *)(dl->file + ehdr->e_shoff);
+
+    uint32_t required_size = 0;
+
+    for (int i = 0; i < ehdr->e_shnum; i++) {
+        if (shdr[i].sh_type == SHT_PROGBITS) {
+            required_size = shdr[i].sh_addr + shdr[i].sh_size;
+        }
+    }
+    
+    printf("required_size = %d\n", required_size);
+
+    dl->mem = (void *) c_mem_alloc(required_size, 0x1000, 6);
+
+    for (int i = 0; i < ehdr->e_shnum; i++) {
+        if (shdr[i].sh_type == SHT_PROGBITS) {
+            printf("loading section %d at %p\n", i, dl->mem + shdr[i].sh_addr);
+            memcpy(dl->mem + shdr[i].sh_addr, dl->file + shdr[i].sh_offset, shdr[i].sh_size);
+        }
+    }
+
+    return 0;
+}
+
 void *dlopen(const char *filename, int flag) {
     dl_t *dl = malloc(sizeof(dl_t));
     
@@ -87,33 +116,34 @@ void *dlopen(const char *filename, int flag) {
     fseek(file, 0, SEEK_END);
     dl->size = ftell(file);
     fseek(file, 0, SEEK_SET);
-    dl->data = malloc(dl->size);
+    dl->file = malloc(dl->size);
 
-    fread(dl->data, 1, dl->size, file);
+    fread(dl->file, 1, dl->size, file);
     fclose(file);
 
-    if (dl->size < sizeof(Elf32_Ehdr) || !is_valid_elf(dl->data)) {
-        free(dl->data);
+    if (dl->size < sizeof(Elf32_Ehdr) || !is_valid_elf(dl->file)) {
+        free(dl->file);
         free(dl);
         return NULL;
     }
+
+    load_sections(dl);
 
     return dl;
 }
 
 void *dlsym(void *handle, const char *symbol) {
     dl_t *dl = (dl_t *)handle;
-    Elf32_Ehdr *ehdr = (Elf32_Ehdr *)dl->data;
+    Elf32_Ehdr *ehdr = (Elf32_Ehdr *)dl->file;
 
-    Elf32_Shdr *shdr = (Elf32_Shdr *)(dl->data + ehdr->e_shoff);
+    Elf32_Shdr *shdr = (Elf32_Shdr *)(dl->file + ehdr->e_shoff);
     uint8_t *dymsym = NULL;
     uint8_t *dynstr = NULL;
 
     for (int i = 0; i < ehdr->e_shnum; i++) {
-        printf("shdr[%d].sh_type = %d\n", i, shdr[i].sh_type);
         if (shdr[i].sh_type == 11) {
-            dymsym = dl->data + shdr[i].sh_offset;
-            dynstr = dl->data + shdr[shdr[i].sh_link].sh_offset;
+            dymsym = dl->file + shdr[i].sh_offset;
+            dynstr = dl->file + shdr[shdr[i].sh_link].sh_offset;
             break;
         }
     }
@@ -123,7 +153,7 @@ void *dlsym(void *handle, const char *symbol) {
         return NULL;
     }
 
-    printf("dymsym = %p, dynstr = %p\n", dymsym - dl->data, dynstr - dl->data);
+    printf("dymsym = %p, dynstr = %p\n", dymsym - dl->file, dynstr - dl->file);
 
     for (uint8_t *p = dymsym; p < dymsym + shdr[1].sh_size; p += sizeof(Elf32_Sym)) {
         Elf32_Sym *sym = (Elf32_Sym *)p;
@@ -134,7 +164,7 @@ void *dlsym(void *handle, const char *symbol) {
             printf("sym->st_info = %d\n", sym->st_info);
             printf("sym->st_other = %d\n", sym->st_other);
             printf("sym->st_shndx = %d\n", sym->st_shndx);
-            return (void *)((uint8_t *) sym->st_value + (uint32_t) dl->data - 0x1000);
+            return (void *)(dl->mem + sym->st_value);
         }
     }
 
@@ -144,7 +174,7 @@ void *dlsym(void *handle, const char *symbol) {
 
 int dlclose(void *handle) {
     dl_t *dl = (dl_t *)handle;
-    free(dl->data);
+    free(dl->file);
     free(dl);
     return 0;
 }
