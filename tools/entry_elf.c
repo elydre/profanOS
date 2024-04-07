30af4758bb6454e01ccc7623f5e95b46e0d10eaa
pf4 <74263128+elydre@users.noreply.github.com>
2024-04-07 12:14:17 +0200

    Update entry_elf.c

--- tools/entry_elf.c
+++ tools/entry_elf.c
@@ -1,10 +1,28 @@
 // standard entry point for ELF compiled files
 
-#include <stdlib.h>
+#include <setjmp.h>
 
 extern int main(int argc, char **argv, char **envp);
 
+extern void __init_libc(char **envp, void *exit_func);
+extern void __exit_libc();
+
+void __entry_exit(int ret);
+
+jmp_buf env;
+
 int entry(int argc, char **argv, char **envp) {
-    init_environ_ptr(envp);
-    return main(argc, argv, envp);
+    __init_libc(envp, __entry_exit);
+
+    int val = setjmp(env);
+
+    if (val == 0)
+        val = main(argc, argv, envp) + 1;
+    __exit_libc();
+
+    return val - 1;
+}
+
+void __entry_exit(int ret) {
+    longjmp(env, ret + 1);
 }
