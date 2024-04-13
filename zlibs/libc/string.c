620428929a625bda897cf95978dfb99a3bf3b7b1
pf4 <74263128+elydre@users.noreply.github.com>
2024-04-13 13:18:35 +0200

    Update string.c

--- zlibs/libc/string.c
+++ zlibs/libc/string.c
@@ -604,9 +604,43 @@ char *strstr(register char *string, char *substring) {
     return NULL;
 }
 
-char *strtok(char *restrict s1, const char *restrict s2) {
-    puts("strtok not implemented yet, WHY DO YOU USE IT ?");
-    return NULL;
+char *strtok(register char *s, register const char *delim) {
+    register char *spanp;
+    register int c, sc;
+    static char *last;
+    char *tok;
+
+    if (s == NULL && (s = last) == NULL)
+        return (NULL);
+
+cont:
+    c = *s++;
+    for (spanp = (char *)delim; (sc = *spanp++) != 0;) {
+        if (c == sc)
+            goto cont;
+    }
+
+    if (c == 0) {       // no non-delimiter characters
+        last = NULL;
+        return (NULL);
+    }
+    tok = s - 1;
+
+    for (;;) {
+        c = *s++;
+        spanp = (char *)delim;
+        do {
+            if ((sc = *spanp++) == c) {
+                if (c == 0)
+                    s = NULL;
+                else
+                    s[-1] = 0;
+                last = s;
+                return (tok);
+            }
+        } while (sc != 0);
+    }
+    // NOTREACHED
 }
 
 char *strtok_r(char *restrict s1, const char *restrict s2,
