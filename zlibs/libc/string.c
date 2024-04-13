e72beb4af1d79603fc8addcdc43e564ac50fb2e3
pf4 <74263128+elydre@users.noreply.github.com>
2024-04-13 12:22:09 +0200

    string: strchrnul + strcspn

--- zlibs/libc/string.c
+++ zlibs/libc/string.c
@@ -286,8 +286,60 @@ char *strchr(const char *p, int ch) {
     }
 }
 
-char *strchrnul(register const char *s, int c) {
-    puts("strchrnul not implemented yet, WHY DO YOU USE IT ?");
+char *strchrnul(const char *s, int c_in) {
+    const unsigned char *char_ptr;
+    const unsigned long int *longword_ptr;
+    unsigned long int longword, magic_bits, charmask;
+    unsigned char c;
+
+    c = (unsigned char) c_in;
+
+    for (char_ptr = (const unsigned char *) s;
+            ((unsigned long int) char_ptr & (sizeof (longword) - 1)) != 0; ++char_ptr)
+        if (*char_ptr == c || *char_ptr == '\0')
+            return (void *) char_ptr;
+
+    longword_ptr = (unsigned long int *) char_ptr;
+
+    magic_bits = -1;
+    magic_bits = magic_bits / 0xff * 0xfe << 1 >> 1 | 1;
+
+    charmask = c | (c << 8);
+    charmask |= charmask << 16;
+    if (sizeof (longword) > 4)
+        charmask |= (charmask << 16) << 16;
+    if (sizeof (longword) > 8)
+        abort ();
+
+    for (;;) {
+      longword = *longword_ptr++;
+
+        if ((((longword + magic_bits) ^ ~longword) & ~magic_bits) != 0 ||
+          ((((longword ^ charmask) + magic_bits) ^ ~(longword ^ charmask))
+            & ~magic_bits) != 0) {
+        const unsigned char *cp = (const unsigned char *) (longword_ptr - 1);
+
+        if (*cp == c || *cp == '\0')
+            return (char *) cp;
+        if (*++cp == c || *cp == '\0')
+            return (char *) cp;
+        if (*++cp == c || *cp == '\0')
+            return (char *) cp;
+        if (*++cp == c || *cp == '\0')
+            return (char *) cp;
+        if (sizeof (longword) > 4) {
+            if (*++cp == c || *cp == '\0')
+                return (char *) cp;
+            if (*++cp == c || *cp == '\0')
+                return (char *) cp;
+            if (*++cp == c || *cp == '\0')
+                return (char *) cp;
+            if (*++cp == c || *cp == '\0')
+                return (char *) cp;
+            }
+        }
+    }
+
     return NULL;
 }
 
@@ -309,8 +361,19 @@ char *strcpy(char *restrict s1, const char *restrict s2) {
     return s1;
 }
 
-size_t strcspn(const char *s1, const char *s2) {
-    puts("strcspn not implemented yet, WHY DO YOU USE IT ?");
+size_t strcspn(const char *s1, register const char *s2) {
+    register const char *p, *spanp;
+    register char c, sc;
+
+    for (p = s1;;) {
+        c = *p++;
+        spanp = s2;
+        do {
+            if ((sc = *spanp++) == c)
+                return (p - 1 - s1);
+        } while (sc != 0);
+    }
+    // NOTREACHED
     return 0;
 }
 
