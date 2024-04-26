69c024028fc846b151b5f1fb6b3bbd162a762912
pf4 <74263128+elydre@users.noreply.github.com>
2024-04-26 17:51:55 +0200

    Update stdlib.c

--- zlibs/libc/stdlib.c
+++ zlibs/libc/stdlib.c
@@ -332,8 +332,7 @@ int jrand48_r(unsigned short int xsubi[3], struct drand48_data *buffer, long int
 
 // FROM THE STDLIB :
 /* Conversion table.  */
-static const char conv_table[64] =
-{
+static const char conv_table[64] = {
   '.', '/', '0', '1', '2', '3', '4', '5',
   '6', '7', '8', '9', 'A', 'B', 'C', 'D',
   'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L',
@@ -1119,8 +1118,7 @@ void I_swap(char *x, char *y) {
 }
 
 // Function to reverse `buffer[i..j]`
-char *I_reverse(char *buffer, int i, int j)
-{
+static char *I_reverse(char *buffer, int i, int j) {
     while (i < j) {
         I_swap(&buffer[i++], &buffer[j--]);
     }
