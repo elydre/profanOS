a13143ec9ac474d9f6f7c1f07f83feef89ee791f
pf4 <74263128+elydre@users.noreply.github.com>
2024-12-23 13:31:56 +0100

    Update dlfcn.h

--- include/zlibs/dlfcn.h
+++ include/zlibs/dlfcn.h
@@ -12,13 +12,15 @@
 #ifndef _DLFCN_H
 #define _DLFCN_H
 
-#define RTLD_LAZY 0
-#define RTLD_NOW 1
-#define RTLD_GLOBAL 2
-#define RTLD_LOCAL 3
+#define RTLD_LAZY     0
+#define RTLD_NOW      1
+#define RTLD_GLOBAL   2
+#define RTLD_LOCAL    3
 #define RTLD_NODELETE 4
-#define RTLD_NOLOAD 5
-#define RTLD_FATAL 6
+#define RTLD_NOLOAD   5
+#define RTLD_FATAL    6
+
+#define RTLD_DEFAULT ((void *) 0)
 
 void *dlopen(const char *filename, int flag);
 void *dlsym(void *handle, const char *symbol);
