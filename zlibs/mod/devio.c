151b273f616b5c62ed05078caf09013e7b54ef0b
pf4 <74263128+elydre@users.noreply.github.com>
2025-01-07 19:08:50 +0100

    Update devio.c

--- zlibs/mod/devio.c
+++ zlibs/mod/devio.c
@@ -251,9 +251,10 @@ int dev_stderr(int id, void *buffer, uint32_t size, uint8_t mode) {
 
 int init_devio(void) {
     return (
-        IS_SID_NULL(fu_fctf_create(0, "/dev/null",   dev_null))      ||
-        IS_SID_NULL(fu_fctf_create(0, "/dev/zero",   dev_zero))      ||
-        IS_SID_NULL(fu_fctf_create(0, "/dev/urandom", dev_rand))      ||
+        IS_SID_NULL(fu_fctf_create(0, "/dev/null",    dev_null))     ||
+        IS_SID_NULL(fu_fctf_create(0, "/dev/zero",    dev_zero))     ||
+        IS_SID_NULL(fu_fctf_create(0, "/dev/random",  dev_rand))     ||
+        IS_SID_NULL(fu_fctf_create(0, "/dev/urandom", dev_rand))     ||
 
         IS_SID_NULL(fu_fctf_create(0, "/dev/kterm",   dev_kterm))    ||
         IS_SID_NULL(fu_fctf_create(0, "/dev/panda",   dev_panda))    ||
