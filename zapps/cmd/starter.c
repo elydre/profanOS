c653ec3cdd92ee6771a8b50d41aa431c89e010ec
pf4 <74263128+elydre@users.noreply.github.com>
2025-02-02 18:26:22 +0100

    Update starter.c

--- zapps/cmd/starter.c
+++ zapps/cmd/starter.c
@@ -9,126 +9,65 @@
 |   === elydre : https://github.com/elydre/profanOS ===         #######  \\   |
 \*****************************************************************************/
 
-#include <profan/filesys.h>
-#include <profan.h>
-
 #include <string.h>
 #include <unistd.h>
 #include <stdlib.h>
+#include <profan.h>
 #include <stdio.h>
 
-void free_tab(char **tab) {
-    for (int i = 0; tab[i]; i++)
-        free(tab[i]);
-    free(tab);
-}
-
-char **ft_split(char *s, char c) {
-    int i, j, k;
+char **ft_split(char *s, int *c) {
+    int i, k;
     char **res;
 
     res = calloc(strlen(s) + 1, sizeof(char *));
-    for (i = j = k = 0; s[i]; i++) {
-        if (s[i] == c) {
-            res[j++] = strndup(s + k, i - k);
+    for (*c = i = k = 0; s[i]; i++) {
+        if (s[i] == ' ') {
+            res[(*c)++] = strndup(s + k, i - k);
             k = i + 1;
         }
     }
     if (k != i)
-        res[j++] = strndup(s + k, i - k);
+        res[(*c)++] = strndup(s + k, i - k);
     return res;
-}
-
-char *find_cmd(char *cmd) {
-    char *path, *res;
-    char **paths;
-    uint32_t sid;
-
-    path = getenv("PATH");
-    if (path == NULL)
-        return NULL;
-    paths = ft_split(path, ':');
-    for (int i = 0; paths[i]; i++) {
-        res = malloc(strlen(paths[i]) + strlen(cmd) + 6);
-        strcpy(res, paths[i]);
-        strcat(res, "/");
-        strcat(res, cmd);
-        strcat(res, ".elf");
-        sid = fu_path_to_sid(SID_ROOT, res);
-        if (!IS_SID_NULL(sid) && fu_is_file(sid)) {
-            free_tab(paths);
-            return res;
-        }
-        free(res);
-    }
-    free_tab(paths);
-    return NULL;
-}
-
-int internal_cd(int argc, char **argv) {
-    if (argc != 2)
-        fprintf(stderr, "Usage: cd <dir>\n");
-    else if (chdir(argv[1]) == -1)
-        fprintf(stderr, "cd: %s: No such file or directory\n", argv[1]);
-    else
-        return 0;
-    return 1;
 }
 
 int execute_line(char *line) {
-    char **args, *cmd;
-    int res, argc;
+    int    argc, res = 1;
+    char **args;
 
-    args = ft_split(line, ' ');
-
-    if (args[0] == NULL)
-        return (free(args), 0);
-
-    for (argc = 0; args[argc]; argc++);
-
-    if (strcmp(args[0], "cd") == 0) {
-        res = internal_cd(argc, args);
-        free_tab(args);
-        return res;
+    if ((args = ft_split(line, &argc))[0] == NULL)
+        res = 0;
+    else if (strcmp(args[0], "cd") == 0) {
+        if (chdir(args[1] ? args[1] : "/") == -1)
+            fputs("cd: No such file or directory\n", stderr);
+        else res = 0;
+    } else if ((line = profan_path_path(args[0], 1))) {
+        res = run_ifexist(line, argc, args);
+        free(line);
+    } else {
+        fprintf(stderr, "%s: Command not found\n", args[0]);
     }
 
-    cmd = find_cmd(args[0]);
-
-    if (cmd == NULL) {
-        fprintf(stderr, "%s: command not found\n", args[0]);
-        free_tab(args);
-        return 1;
-    }
-
-    res = run_ifexist(cmd, argc, args);
-    free_tab(args);
-    free(cmd);
+    for (int i = 0; args[i]; i++)
+        free(args[i]);
+    free(args);
 
     return res;
 }
 
-void remove_trailing_newline(char *s) {
-    int len = strlen(s);
-    if (len > 0 && s[len - 1] == '\n')
-        s[len - 1] = '\0';
-}
-
 int main(void) {
-    char *line;
-    int res = 0;
+    char  *line = NULL;
+    size_t len = 0;
 
-    while (1) {
+    for (int res = 0;; res = execute_line(line)) {
         printf("(%d) %s # ", res, profan_wd_path);
         fflush(stdout);
-        line = profan_input(NULL);
-        if (!line || !line[0]) {
-            putchar('\n');
-            profan_kfree(line);
+        if ((res = getline(&line, &len, stdin)) < 1)
             break;
-        }
-        remove_trailing_newline(line);
-        res = execute_line(line);
-        profan_kfree(line);
+        line[res - 1] = '\0';
     }
+
+    putchar('\n');
+    free(line);
     return 0;
 }
