865c3cada683333fdff08a6ea94c8e39b4fc05ec
pf4 <74263128+elydre@users.noreply.github.com>
2025-02-04 19:22:56 +0100

    Update mouse.c

--- zapps/games/mouse.c
+++ zapps/games/mouse.c
@@ -19,48 +19,53 @@
 #include <unistd.h>
 #include <stdio.h>
 
+uint32_t *fb;
+uint32_t pitch;
+int width;
+int height;
+
+char data[21][12] = {
+    {1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+    {1, 2, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+    {1, 2, 2, 1, 0, 0, 0, 0, 0, 0, 0, 0},
+    {1, 2, 2, 2, 1, 0, 0, 0, 0, 0, 0, 0},
+    {1, 2, 2, 2, 2, 1, 0, 0, 0, 0, 0, 0},
+    {1, 2, 2, 2, 2, 2, 1, 0, 0, 0, 0, 0},
+    {1, 2, 2, 2, 2, 2, 2, 1, 0, 0, 0, 0},
+    {1, 2, 2, 2, 2, 2, 2, 2, 1, 0, 0, 0},
+    {1, 2, 2, 2, 2, 2, 2, 2, 2, 1, 0, 0},
+    {1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 1, 0},
+    {1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 1},
+    {1, 2, 2, 2, 2, 2, 2, 1, 1, 1, 1, 1},
+    {1, 2, 2, 2, 1, 2, 2, 1, 0, 0, 0, 0},
+    {1, 2, 2, 1, 1, 2, 2, 1, 0, 0, 0, 0},
+    {1, 2, 1, 0, 0, 1, 2, 2, 1, 0, 0, 0},
+    {1, 1, 0, 0, 0, 1, 2, 2, 1, 0, 0, 0},
+    {0, 0, 0, 0, 0, 0, 1, 2, 2, 1, 0, 0},
+    {0, 0, 0, 0, 0, 0, 1, 2, 2, 1, 0, 0},
+    {0, 0, 0, 0, 0, 0, 0, 1, 2, 2, 1, 0},
+    {0, 0, 0, 0, 0, 0, 0, 1, 2, 2, 1, 0},
+    {0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0},
+};
+
+uint32_t old[21][12];
+
 void draw_mouse(int x, int y) {
-    int red = 0xFF0000;
-    int white = 0xFFFFFF;
-
-    int data[21][12] = {
-        {1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-        {1, 2, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-        {1, 2, 2, 1, 0, 0, 0, 0, 0, 0, 0, 0},
-        {1, 2, 2, 2, 1, 0, 0, 0, 0, 0, 0, 0},
-        {1, 2, 2, 2, 2, 1, 0, 0, 0, 0, 0, 0},
-        {1, 2, 2, 2, 2, 2, 1, 0, 0, 0, 0, 0},
-        {1, 2, 2, 2, 2, 2, 2, 1, 0, 0, 0, 0},
-        {1, 2, 2, 2, 2, 2, 2, 2, 1, 0, 0, 0},
-        {1, 2, 2, 2, 2, 2, 2, 2, 2, 1, 0, 0},
-        {1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 1, 0},
-        {1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 1},
-        {1, 2, 2, 2, 2, 2, 2, 1, 1, 1, 1, 1},
-        {1, 2, 2, 2, 1, 2, 2, 1, 0, 0, 0, 0},
-        {1, 2, 2, 1, 1, 2, 2, 1, 0, 0, 0, 0},
-        {1, 2, 1, 0, 0, 1, 2, 2, 1, 0, 0, 0},
-        {1, 1, 0, 0, 0, 1, 2, 2, 1, 0, 0, 0},
-        {0, 0, 0, 0, 0, 0, 1, 2, 2, 1, 0, 0},
-        {0, 0, 0, 0, 0, 0, 1, 2, 2, 1, 0, 0},
-        {0, 0, 0, 0, 0, 0, 0, 1, 2, 2, 1, 0},
-        {0, 0, 0, 0, 0, 0, 0, 1, 2, 2, 1, 0},
-        {0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0},
-    };
-
-    uint32_t pitch = syscall_vesa_pitch();
-    uint32_t *fb = syscall_vesa_fb();
-
     // draw the mouse
     for (int i = 0; i < 21; i++) {
         for (int j = 0; j < 12; j++) {
+            if (x + j < 0 || x + j >= width || y + i < 0 || y + i >= height)
+                continue;
             switch (data[i][j]) {
                 case 0:
                     break;
                 case 1:
-                    fb[(y + i) * pitch + (x + j)] = red;
+                    old[i][j] = fb[(y + i) * pitch + (x + j)];
+                    fb[(y + i) * pitch + (x + j)] = 0xFFFFFF;
                     break;
                 case 2:
-                    fb[(y + i) * pitch + (x + j)] = white;
+                    old[i][j] = fb[(y + i) * pitch + (x + j)];
+                    fb[(y + i) * pitch + (x + j)] = 0x333333;
                     break;
             }
 
@@ -68,18 +73,32 @@ void draw_mouse(int x, int y) {
     }
 }
 
-int main(void) {
-    printf("\e[2J");
+void restore_mouse(int x, int y) {
+    for (int i = 0; i < 21; i++) {
+        for (int j = 0; j < 12; j++) {
+            if (x + j < 0 || x + j >= width || y + i < 0 || y + i >= height)
+                continue;
+            if (data[i][j] != 0)
+                fb[(y + i) * pitch + (x + j)] = old[i][j];
+        }
+    }
+}
 
+int main(void) {
     int x, y;
+
+    pitch = syscall_vesa_pitch();
+    fb = syscall_vesa_fb();
+
+    width = syscall_vesa_width();
+    height = syscall_vesa_height();
 
     while (1) {
         x = mouse_get_x();
         y = mouse_get_y();
-        // we draw a rectangle
-        printf("x: %d, y: %d, %d %d %d\n", x, y, mouse_get_button(0), mouse_get_button(1), mouse_get_button(2));
         draw_mouse(x, y);
-        usleep(25000);
+        usleep(10000);
+        restore_mouse(x, y);
     }
     return 0;
 }
