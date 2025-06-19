e73905fcbfe6dafa44ac3c246d4eb9aa8226348b
asqel <91128487+asqel@users.noreply.github.com>
2025-06-19 23:40:59 +0200

    pni_recv return struct

--- zlibs/libpni/recv.c
+++ zlibs/libpni/recv.c
@@ -17,59 +17,56 @@ static int check_flags(uint32_t flags, flags_info_t *info, va_list *args) {
 	return 0;
 }
 
-static uint8_t *parse_packet(uint8_t *raw_packet, int raw_len, int *len) {
+static pni_packet_t parse_packet(uint8_t *raw_packet, int raw_len) {
+	pni_packet_t packet = {0};
+	packet.len = -1;
 	if (raw_len < (int)sizeof(ethernet_header_t) + (int)sizeof(ip_header_t) + (int)sizeof(udp_header_t))
-		return NULL;
+		return packet;
 
 	ethernet_header_t *eth = (ethernet_header_t *)raw_packet;
 	if (ntohs(eth->ether_type) != 0x0800)
-		return NULL;
+		return packet;
+
 
 	ip_header_t *ip = (ip_header_t *)(raw_packet + sizeof(ethernet_header_t));
 	if (ip->version_ihl != 0x45 || ip->protocol != 17)
-		return NULL;
+		return packet;
 
 	udp_header_t *udp = (udp_header_t *)(raw_packet + sizeof(ethernet_header_t) + sizeof(ip_header_t));
 	int data_len = ntohs(udp->length) - sizeof(udp_header_t);
 	if (data_len <= 0 || data_len > raw_len - (int)sizeof(ethernet_header_t) - (int)sizeof(ip_header_t) - (int)sizeof(udp_header_t))
-		return NULL;
-	if (data_len > PNI_MAX_PACKET)
-		return NULL;
+		return packet;
 
 	uint8_t *data = malloc(data_len);
 	if (!data)
-		return NULL;
+		return packet;
 	memcpy(data, raw_packet + sizeof(ethernet_header_t) + sizeof(ip_header_t) + sizeof(udp_header_t), data_len);
 
-	*len = data_len;
-	return data;
+	packet.src_port = ntohs(udp->src_port);
+	packet.dest_port = ntohs(udp->dest_port);
+	memcpy(packet.src_ip, ip->src_ip, sizeof(packet.src_ip));
+	packet.len = data_len;
+	packet.data = data;
+	return packet;
 }
 
-uint8_t *pni_recv(int *len, uint32_t flags, ...) {
+pni_packet_t pni_recv(uint32_t flags, ...) {
 	va_list args;
 	va_start(args, flags);
 	flags_info_t info = {0};
-	if (check_flags(flags, &info, &args)) {
-		*len = PNI_ERR_FLAGS;
-		return NULL;
-	}
+	if (check_flags(flags, &info, &args))
+		return (pni_packet_t){.len = PNI_ERR_FLAGS, .data = NULL};
 	va_end(args);
-	if (pni_inited == 0) {
-		*len = PNI_ERR_NO_INIT;
-		return NULL;
-	}
+	if (pni_inited == 0)
+		return (pni_packet_t){.len = PNI_ERR_NO_INIT, .data = NULL};
 	uint32_t time_end = syscall_timer_get_ms() + info.hang_time;
 	while (1) {
 		int ready = syscall_eth_listen_isready();
-		if (!ready && (flags & PNI_RECV_NOHANG)) {
-			*len = PNI_ERR_NO_PACKET;
-			return NULL;
-		}
+		if (!ready && (flags & PNI_RECV_NOHANG))
+			return (pni_packet_t){.len = PNI_ERR_NO_PACKET, .data = NULL};
 		if (!ready && (flags & PNI_RECV_HANGTIME)) {
-			if (syscall_timer_get_ms() >= time_end) {
-				*len = PNI_ERR_NO_PACKET;
-				return NULL;
-			}
+			if (syscall_timer_get_ms() >= time_end)
+				return (pni_packet_t){.len = PNI_ERR_NO_PACKET, .data = NULL};
 			usleep(500);
 			continue;
 		}
@@ -81,11 +78,11 @@ uint8_t *pni_recv(int *len, uint32_t flags, ...) {
 		uint8_t *raw_packet = malloc(raw_len);
 		syscall_eth_listen_get(raw_packet);
 
-		uint8_t *res = parse_packet(raw_packet, raw_len, len);
+		pni_packet_t res = parse_packet(raw_packet, raw_len);
 		free(raw_packet);
-		if (res == NULL)
+		if (res.len == -1)
 			continue;
+
 		return res;
 	}
-	return NULL;
 }
