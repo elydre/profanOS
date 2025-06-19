07d5499f6ddc08d39deb45e0c6ac27a779070e66
pf4 <74263128+elydre@users.noreply.github.com>
2025-06-20 01:16:07 +0200

    Update tftp.c

--- zapps/f/tftp.c
+++ zapps/f/tftp.c
@@ -1,3 +1,14 @@
+/*****************************************************************************\
+|   === tftp.c : 2025 ===                                                     |
+|                                                                             |
+|    -                                                             .pi0iq.    |
+|                                                                 d"  . `'b   |
+|    This file is part of profanOS and is released under          q. /|\  "   |
+|    the terms of the GNU General Public License                   `// \\     |
+|                                                                  //   \\    |
+|   === elydre : https://github.com/elydre/profanOS ===         #######  \\   |
+\*****************************************************************************/
+
 // @LINK: libpf, libpni
 
 #include <stdio.h>
@@ -8,21 +19,46 @@
 #include <profan/carp.h>
 #include <unistd.h>
 
-#define TFTP_PORT 25565
-#define MODE "octet"
-#define MAXL 100
+#define TFTP_PORT 69
+#define ANSI_UPCLEAR "\e[1A\e[2K"
 
-uint16_t src_port, dest_port;
+in_port_t src_port, dest_port;
+in_addr_t server_ip;
+int display_mode = 0;
 
 #define DEBUG(x) do { if (x != 0) { \
     fprintf(stderr, "Error at line %d: %s\n", __LINE__, x); \
 } } while (0)
 
+void send_ack(in_port_t dest, uint16_t block_num) {
+    uint8_t packet[4];
+    uint16_t tmp;
+
+    tmp = htons(4); // ACK opcode
+    memcpy(packet, &tmp, 2);
+
+    tmp = htons(block_num); // Block number
+    memcpy(packet + 2, &tmp, 2);
+
+    DEBUG(pni_send(src_port, dest, packet, sizeof(packet), (uint8_t *) server_ip));
+}
+
+void send_rrq(const char *filename) {
+    uint8_t packet[516];
+    uint16_t opcode = htons(1);
+    int len = strlen(filename);
+
+    memcpy(packet, &opcode, 2);
+    strlcpy((char *) packet + 2, filename, 500);
+    strcpy((char *) packet + 3 + len, "octet");
+
+    DEBUG(pni_send(src_port, dest_port, packet, len + 9, (uint8_t *) server_ip));
+}
 
 /*
 void send_data(uint8_t *addr, uint16_t block_num, const char *data, size_t len) {
     char packet[516];
-    uint16_t opcode = htons(3); 
+    uint16_t opcode = htons(3);
     memcpy(packet, &opcode, 2);
     uint16_t blocknum = block_num;
     memcpy(packet + 2, &blocknum, 2);
@@ -49,20 +85,20 @@ void sendWRQ(uint8_t *server, const char *filename) {
 void PutFile(uint8_t *server_addr, const char *fileName) {
     sendWRQ(server_addr, fileName);
 
-    int recv_len;
-    uint8_t *buffer = pni_recv(&recv_len, 0);
-    printf("Received packet of length: %d\n", recv_len);
+    int packet.len;
+    uint8_t *packet.data = pni_recv(&packet.len, 0);
+    printf("Received packet of length: %d\n", packet.len);
 
-    if (recv_len < 4) {
+    if (packet.len < 4) {
         // fprintf(stderr, "Invalid acknowledgment packet\n");
         return;
     }
 
-    uint16_t opcode = ntohs(*(uint16_t*)(buffer));
-    uint16_t block_num_ack = ntohs(*(uint16_t*)(buffer + 2));
+    uint16_t opcode = ntohs(*(uint16_t*)(packet.data));
+    uint16_t block_num_ack = ntohs(*(uint16_t*)(packet.data + 2));
 
     if (opcode == 5) {
-        fprintf(stderr, "Server Error: %s\n", buffer + 4);
+        fprintf(stderr, "Server Error: %s\n", packet.data + 4);
         return;
     } else if (opcode != 4 || block_num_ack != 0) {
         fprintf(stderr, "Unexpected opcode or block number (opcode: %d, block_num: %d)\n", opcode, block_num_ack);
@@ -84,19 +120,19 @@ void PutFile(uint8_t *server_addr, const char *fileName) {
         send_data(server_addr, block_num, data, read_len);
         printf("Sent DATA packet for block: %d\n", block_num);
 
-        buffer = pni_recv(&recv_len, 0);
+        packet.data = pni_recv(&packet.len, 0);
 
-        printf("Received ACK packet of length: %d\n", recv_len);
-        if (recv_len < 4) {
+        printf("Received ACK packet of length: %d\n", packet.len);
+        if (packet.len < 4) {
             // fprintf(stderr, "Invalid ACK packet\n");
             break;
         }
 
-        opcode = ntohs(*(uint16_t*)(buffer));
-        block_num_ack = ntohs(*(uint16_t*)(buffer + 2));
+        opcode = ntohs(*(uint16_t*)(packet.data));
+        block_num_ack = ntohs(*(uint16_t*)(packet.data + 2));
 
         if (opcode == 5) {
-            fprintf(stderr, "Server Error: %s\n", buffer + 4);
+            fprintf(stderr, "Server Error: %s\n", packet.data + 4);
             break;
         } else if (opcode != 4 || block_num_ack != block_num) {
             fprintf(stderr, "Unexpected ACK (opcode: %d, block_num: %d)\n", opcode, block_num_ack);
@@ -117,33 +153,12 @@ void PutFile(uint8_t *server_addr, const char *fileName) {
 }
 */
 
-void send_ack(uint8_t *serv_addr, uint16_t block_num) {
-    uint8_t packet[4];
-    uint16_t opcode = htons(4);
-    uint16_t block_len = htons(block_num);
-    memcpy(packet, &opcode, 2);
-    memcpy(packet + 2, &block_len, 2);
-    DEBUG(pni_send(src_port, pni_dest_ip(), packet, sizeof(packet), serv_addr));
-    printf("Sent ACK for block: %d\n", block_num);
-}
+void getFile(const char *filename) {
+    send_rrq(filename);
 
-void sendRRQ(uint8_t *server, const char *filename) {
-    uint8_t packet[516];
-    uint16_t opcode = htons(1);
-    memcpy(packet, &opcode, 2);
-    strcpy((char *) packet + 2, filename);
-    strcpy((char *) packet + 2 + strlen(filename) + 1, MODE);
-    size_t len = 4 + strlen(filename) + strlen(MODE);
-    DEBUG(pni_send(src_port, dest_port, packet, len, server));
-    printf("Sent RRQ for file: %s\n", filename);
-}
+    pni_packet_t packet;
+    packet.data = NULL;
 
-void getFile(uint8_t *server, const char *filename) {
-    sendRRQ(server, filename);
-
-    int recv_len;
-
-    uint8_t *buffer;
     FILE *file = fopen(filename, "wb");
 
     if (file == NULL) {
@@ -151,129 +166,106 @@ void getFile(uint8_t *server, const char *filename) {
         return;
     }
 
-    uint16_t expected_block = 1;
-    while (1) {
-        // ssize_t recv_len = recvfrom(sock, buffer, sizeof(buffer), 0, (struct sockaddr *)&transfer_addr, &addr_len);
-        buffer = pni_recv(&recv_len, PNI_RECV_HANGTIME, 6000);
-        printf("Received packet of length: %d\n", recv_len);
+    if (display_mode)
+        printf("%s: starting transfer...\n", filename);
 
-        if (recv_len < 4) {
-            // printf("Received packet with invalid length\n");
+    uint16_t expected_block = 1;
+    int retries = 0;
+
+    while (1) {
+        free(packet.data);
+
+        packet = pni_recv(PNI_RECV_HANGTIME, 1000);
+
+        if (packet.len < 4) {
+            fprintf(stderr, "tftp: received packet too short\n");
             break;
         }
 
-        uint16_t opcode = ntohs(*(uint16_t *)(buffer));
-        if (opcode == 3) {
-            uint16_t blocknum = ntohs(*(uint16_t *)(buffer + 2));
-            printf("-> DATA packet for block: %d\n", blocknum);
-
-            if (blocknum == expected_block) {
-                size_t data_len = recv_len - 4;
-                fwrite(buffer + 4, 1, data_len, file);
-                printf("-> Written %u bytes to file\n", data_len);
-                send_ack(server, blocknum);
-
-                expected_block++;
-                if (data_len < 512) {
-                    printf("End of file transfer\n");
-                    break;
-                }
-            } else {
-                printf("Unexpected block number: %d (expected: %d)\n", blocknum, expected_block);
-            }
-        } else if (opcode == 5) {
-            fprintf(stderr, "-> Error: %s\n", buffer + 4);
+        uint16_t opcode = ntohs(*(uint16_t *)(packet.data));
+        
+        if (opcode == 5) {
+            fprintf(stderr, "tftp: server error: %s\n", packet.data + 4);
             break;
-        } else {
-            printf("Unexpected opcode: %d\n", opcode);
+        } else if (opcode != 3) {
+            fprintf(stderr, "tftp: unexpected opcode %d during receive\n", opcode);
+            break;
+        }
+
+        uint16_t blocknum = ntohs(*(uint16_t *)(packet.data + 2));
+
+        if (blocknum != expected_block) {
+            if (retries >= 10) {
+                fprintf(stderr, "tftp: too many retries, aborting transfer\n");
+                break;
+            }
+
+            if (display_mode)
+                printf(ANSI_UPCLEAR "%s: wrong block number, retry...\n", filename);
+            
+            // Resend ACK for the expected block number
+            if (blocknum < expected_block)
+                send_ack(dest_port, expected_block);
+            
+            retries++;
+            continue;
+        }
+
+        retries = 0;
+
+        size_t data_len = packet.len - 4;
+        fwrite(packet.data + 4, 1, data_len, file);
+
+        if (display_mode)
+            printf(ANSI_UPCLEAR"%s: %d bytes (block %d)\n", filename, blocknum * 512, blocknum);
+
+        send_ack(packet.src_port, blocknum);
+        expected_block++;
+
+        if (data_len < 512) {
+            printf(ANSI_UPCLEAR"%s: transfer complete, %d bytes\n", filename, ftell(file));
             break;
         }
     }
 
+    free(packet.data);
     fclose(file);
 }
 
-int str_to_ip(const char *src, unsigned char *dst) {
-    int saw_digit, octets, ch;
-    unsigned char tmp[4], *tp;
-    saw_digit = 0;
-    octets = 0;
-    *(tp = tmp) = 0;
-    while (*src) {
-        ch = *src++;
-        if (ch >= '0' && ch <= '9') {
-            unsigned int new = *tp * 10 + (ch - '0');
-            if (saw_digit && *tp == 0)
-                return 1;
-            if (new > 255)
-                return 1;
-            *tp = new;
-            if (!saw_digit) {
-                if (++octets > 4)
-                    return 1;
-                saw_digit = 1;
-            }
-        }
-        else if (ch == '.' && saw_digit) {
-            if (octets == 4)
-                return 1;
-            *++tp = 0;
-            saw_digit = 0;
-        }
-        else
-            return 1;
+in_port_t parse_port(char lettre) {
+    int port = carp_get_int(lettre);
+    if (port == -1)
+        return TFTP_PORT;
+    if (port < 0 || port > 65535) {
+        fprintf(stderr, "Invalid port number: %d\n", port);
+        exit(1);
     }
-    if (octets < 4)
-        return 1;
-    memcpy(dst, tmp, 4);
-    return 0;
+    return (in_port_t) port;
 }
 
 int main(int argc, char **argv) {
-    carp_init("[-l <local_port>] [-r <remote_port>] [-s] <server_ip> <filename>", CARP_FMIN(2) | 2);
+    carp_init("[-l <local_port>] [-p <remote_port>] [-q] [-n] <server_ip> <filename>", CARP_FMIN(2) | 2);
 
     carp_register('l', CARP_NEXT_INT, "local port (default: 69)");
-    carp_register('r', CARP_NEXT_INT, "remote port (default: 69)");
+    carp_register('p', CARP_NEXT_INT, "remote port (default: 69)");
+    carp_register('q', CARP_STANDARD, "quiet, no transfer information");
     carp_register('s', CARP_STANDARD, "send file instead of receiving it");
 
     if (carp_parse(argc, argv)) {
         return 1;
     }
 
-    int tmp;
-
-    tmp = carp_get_int('l');
-    if (tmp != -1) {
-        if (tmp < 0 || tmp > 65535) {
-            fprintf(stderr, "Invalid local port: %d\n", tmp);
-            return 1;
-        }
-        src_port = tmp;
-    } else {
-        src_port = TFTP_PORT;
-    }
-
-    tmp = carp_get_int('r');
-    if (tmp != -1) {
-        if (tmp < 0 || tmp > 65535) {
-            fprintf(stderr, "Invalid remote port: %d\n", tmp);
-            return 1;
-        }
-        dest_port = tmp;
-    } else {
-        dest_port = TFTP_PORT;
-    }
+    src_port  = parse_port('l');
+    dest_port = parse_port('p');    
 
     const char **args = carp_get_files();
 
-    uint8_t server_ip[4];
+    display_mode = !(carp_isset('q') || !isatty(STDOUT_FILENO));
 
-    if (str_to_ip(args[0], server_ip) != 0) {
-        fprintf(stderr, "Invalid server IP address: %s\n", args[0]);
+    if (inet_aton(args[0], (void *) &server_ip) == 0) {
+        fprintf(stderr, "tftp: invalid server IP address: %s\n", args[0]);
         return 1;
     }
-
-    printf("Using server IP: %d.%d.%d.%d\n", server_ip[0], server_ip[1], server_ip[2], server_ip[3]);
 
     if (pni_init() != 0) {
         fprintf(stderr, "Failed to initialize PNI\n");
@@ -281,10 +273,9 @@ int main(int argc, char **argv) {
     }
 
     if (carp_isset('s')) {
-        // PutFile(server_ip, args[1]);
         printf("Sending file is not implemented yet.\n");
     } else {
-        getFile(server_ip, args[1]);
+        getFile(args[1]);
     }
 
     pni_exit();
