0f12c7b712dcfdb00dd618b3083ec7ab9c62893b
asqel <91128487+asqel@users.noreply.github.com>
2025-09-10 02:48:07 +0200

    doc?

--- kernel/net/packets.c
+++ kernel/net/packets.c
@@ -2,6 +2,29 @@
 #include <minilib.h>
 #include <kernel/process.h>
 
+/*
+
+struct eth_info_t {
+	uint32_t net_mask;
+	uint32_t router_ip;
+	uint8_t router_mac[6];
+
+	uint32_t ip;
+	uint32_t mac;
+};
+
+syscalls:
+	int eth_start(int type) -> returns a id of the listner (can have multiple by process)
+	void eth_end(int id)
+	void eth_send(void *data, int len)
+	int eth_is_ready(int id) -> -1 if not ready to recv, else returns the size of the next packet
+	void eth_recv(int id, void *data)
+	void eth_get_info(int id, struct eth_info_t *info)
+	void eth_set_info(int id, struct eth_info_t *info)
+
+
+*/
+
 #define LISTENERS_MAX_PACKET 255
 
 typedef struct {
