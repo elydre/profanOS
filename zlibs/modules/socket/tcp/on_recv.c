47e259a1f014b3658dfde3ebc6405896f2d3bbe9
asqel <axel.lleres@gmail.com>
2026-05-24 00:57:13 +0200

    aaaaa la stack

--- zlibs/modules/socket/tcp/on_recv.c
+++ zlibs/modules/socket/tcp/on_recv.c
@@ -94,14 +94,15 @@ void socket_on_recv_tcp(uint32_t src_ip, uint32_t dest_ip, uint8_t *data, int da
     else if (server_sock)
         tcp_on_packet_recv(server_sock, &packet);
     else {
-		tcp_t tmp;
-		tmp.local_ip = packet.ip_dest;
-		tmp.remote_ip = packet.ip_src;
-		tmp.local_port = packet.port_dest;
-		tmp.remote_port = packet.port_src;
-		tmp.current_seq = packet.ack;
-		tmp.current_ack = packet.seq + packet.data_len;
-		tmp.recv_len = sizeof(tmp.recv);
-		tcp_send_reset(&tmp);
+		// !TODO dont do that it explodes the stack and reboot
+		//tcp_t tmp;
+		//tmp.local_ip = packet.ip_dest;
+		//tmp.remote_ip = packet.ip_src;
+		//tmp.local_port = packet.port_dest;
+		//tmp.remote_port = packet.port_src;
+		//tmp.current_seq = packet.ack;
+		//tmp.current_ack = packet.seq + packet.data_len;
+		//tmp.recv_len = sizeof(tmp.recv);
+		//tcp_send_reset(&tmp);
 	}
 }
