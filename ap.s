7ff5e8ee104694c90f0973e24b308c393beb0ad5
asqel <axel.lleres@gmail.com>
2026-07-19 18:10:48 +0200

    a

--- ap.s
+++ ap.s
@@ -26,8 +26,50 @@ _wait:
     jmp loop
 
 out:
-	hlt
-	jmp $
+	jmp start
 
 msg:
 	db "salut depuis le coeur1", 0x10, 0
+
+PS2_DATA   equ 0x60
+PS2_STATUS equ 0x64
+
+start:
+
+.wait1:
+    in  al, PS2_STATUS
+    test al, 2
+    jnz .wait1
+
+    mov al, 0xED
+    out PS2_DATA, al
+
+.wait_ack1:
+    in  al, PS2_STATUS
+    test al, 1
+    jz .wait_ack1
+
+    in  al, PS2_DATA
+    cmp al, 0xFA
+    jne $
+
+.wait2:
+    in  al, PS2_STATUS
+    test al, 2
+    jnz .wait2
+
+    mov al, 0x04
+    out PS2_DATA, al
+
+.wait_ack2:
+    in  al, PS2_STATUS
+    test al, 1
+    jz .wait_ack2
+
+    in  al, PS2_DATA
+    cmp al, 0xFA
+    jne $
+
+hang:
+	hlt
+    jmp hang
